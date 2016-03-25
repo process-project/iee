c99f51b4fbbd3b82f5bb460aa6adc8ab470d5ef0
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-03-25 11:19:08 +0100

    fixes #13: user confirmation view added, DB seeds updated

--- app/controllers/account_confirmation_controller.rb
+++ app/controllers/account_confirmation_controller.rb
@@ -5,4 +5,65 @@ class AccountConfirmationController < ApplicationController
     @users[:not_confirmed] = User.where(approved: false)
     @user_confirmations = @users[:not_confirmed].exists?
   end
+  
+  def approve
+    user = User.find(params[:id])
+    
+    if user and view_context.supervisor?
+      logger.info "Approving #{user.email} by #{current_user.email}"
+      user.approved = true
+      user.save
+      flash[:notice] = t("user_approved", email: user.email)
+    end
+    
+    redirect_to(action: "index")
+  end
+  
+  def approve_all
+    if view_context.supervisor?
+      logger.info "Approving all pending users by #{current_user.email}"
+      
+      User.where(approved: false).each do |user|
+        user.approved = true
+        user.save
+      end
+      
+      flash[:notice] = t("all_approved")
+    end
+    
+    redirect_to(action: "index")
+  end
+  
+  def block
+    user = User.find(params[:id])
+    
+    if user and view_context.supervisor?
+      if user == current_user
+        flash[:alert] = t("cannot_block_itself")
+      else
+        logger.info "Blocking #{user.email} by #{current_user.email}"
+        user.approved = false
+        user.save
+        flash[:notice] = t("user_blocked", email: user.email)
+      end
+    end
+    
+    redirect_to(action: "index")
+  end
+  
+  def block_all
+    if view_context.supervisor?
+      logger.info "Blocking all non-supervisor and non-admin accounts by #{current_user.email}"
+      
+      User.eager_load(:groups).where("groups.name NOT IN (?) OR groups.id IS NULL",
+          ["admin", "supervisor"]).each do |user|
+        user.approved = false
+        user.save
+      end
+      
+      flash[:notice] = t("all_blocked")
+    end
+    
+    redirect_to(action: "index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: redirect("account_confirmation/index")
 
   get "account_confirmation/index"
+  put "account_confirmation/approve_all"
+  put "account_confirmation/approve/:id", to: "account_confirmation#approve", as: "approve_user"
+  put "account_confirmation/block_all"
+  put "account_confirmation/block/:id", to: "account_confirmation#block", as: "block_user"
 
   devise_for :users,
              controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
