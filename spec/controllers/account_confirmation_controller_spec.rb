013e1af337b644139c09325e8833576ecd01d4f9
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-03-29 13:33:12 +0200

    #13: account confirmation controller tests fixed

--- spec/controllers/account_confirmation_controller_spec.rb
+++ spec/controllers/account_confirmation_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe AccountConfirmationController, type: :controller do
     
     put :block, id: user.id
     
-    expect(response).to redirect_to(account_confirmation_index_path)
+    expect(response).to redirect_to(account_confirmations_index_path)
     expect(flash[:alert]).to eq(I18n.t("cannot_block_itself"))
   end
 end
