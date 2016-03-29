f4397ab9c712824d4ea02a23219f6dc525235a64
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-03-29 13:31:45 +0200

    #13: confirmation urls more resource-like

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: redirect("profile/index")
 
-  get "account_confirmation/index"
-  put "account_confirmation/approve_all"
-  put "account_confirmation/approve/:id", to: "account_confirmation#approve", as: "approve_user"
-  put "account_confirmation/block_all"
-  put "account_confirmation/block/:id", to: "account_confirmation#block", as: "block_user"
+  get "account_confirmations/index", to: "account_confirmation#index"
+  put "account_confirmations", to: "account_confirmation#approve_all"
+  put "account_confirmations/:id", to: "account_confirmation#approve", as: "approve_user"
+  delete "account_confirmations", to: "account_confirmation#block_all"
+  delete "account_confirmations/:id", to: "account_confirmation#block", as: "block_user"
   
   get "profile/index"
 
