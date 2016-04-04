02633783ec3d823b8f4a425a28ed0fe71b381a07
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-04-04 15:54:04 +0200

    Pdp rest API

--- app/controllers/pdp_controller.rb
+++ app/controllers/pdp_controller.rb
@@ -1,2 +1,19 @@
-class PdpController < ApplicationController
+class PdpController < ActionController::Base
+  include Pundit
+
+  protect_from_forgery with: :null_session
+
+  def index
+    head(permit? ? :ok : :forbidden)
+  end
+
+  private
+
+  def permit?
+    current_user && resource && policy(resource).permit?(params[:permission])
+  end
+
+  def resource
+    @resource ||= Resource.find_by(uri: params[:uri])
+  end
 end
