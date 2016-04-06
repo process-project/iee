23b48e6dc9136bb071110e803be86cd9d5799a5c
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-04-06 14:55:23 +0200

    JWT devise strategy
    User can be authenticated using JWT token. Following header need to be
    added to the reuqest:
    
    ```
    Authorization: Bearer <token>
    ```

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require 'devise/strategies/jwt_authenticatable'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -244,10 +246,10 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+  config.warden do |manager|
+    manager.intercept_401 = false
+    manager.default_strategies(scope: :user).unshift :jwt
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
@@ -262,7 +264,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  
+
   Devise::SessionsController.layout "login"
   Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? "application" :
     "login" }
