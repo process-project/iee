d33bda3b4fbed500ad7c65de752513751159f03d
Tomasz Gubala <t.gubala@cyfronet.pl>
2016-04-07 09:49:37 +0200

    rebuilt haml_helpers

--- app/helpers/haml_helper.rb
+++ app/helpers/haml_helper.rb
@@ -1,16 +1,16 @@
 module HamlHelper
-  def form_input_class(input_name, subject)
-    if subject.errors[input_name].present?
+  def form_input_class(resource, input_name)
+    if resource.errors[input_name].present?
       "form-control parsley-error"
     else
       "form-control"
     end
   end
   
-  def show_field_errors(input_name, subject)
-    if subject.errors[input_name].present?
+  def show_field_errors(resource, input_name)
+    if resource.errors[input_name].present?
       haml_tag :ul, :class => "parsley-errors-list filled" do
-        subject.errors[input_name].each do |error|
+        resource.errors[input_name].each do |error|
           haml_tag :li, error, :class => "parsley-required"
         end
       end
