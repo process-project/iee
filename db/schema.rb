055a9898652bd96ae72437a0f15c93eab4787d78
tomek <tomek.bartynski@gmail.com>
2016-04-19 13:29:43 +0200

    run migration to add assoc between Patient and Computation
    https://gitlab.com/eurvalve/vapor/issues/40

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418082419) do
+ActiveRecord::Schema.define(version: 20160418142336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 20160418082419) do
     t.integer  "user_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "patient_id"
   end
+
+  add_index "computations", ["patient_id"], name: "index_computations_on_patient_id", using: :btree
 
   create_table "data_files", force: :cascade do |t|
     t.string   "name",       null: false
@@ -125,5 +128,6 @@ ActiveRecord::Schema.define(version: 20160418082419) do
   add_index "users", ["plgrid_login"], name: "index_users_on_plgrid_login", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "computations", "patients"
   add_foreign_key "data_files", "patients"
 end
