3243e248f10c11fb55b02aede57f920e9cafb59c
Tomasz Gubala <t.gubala@cyfronet.pl>
2016-04-19 16:24:59 +0200

    adding forgotten relation declaration

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,7 @@ class Patient < ActiveRecord::Base
   enum procedure_status: [ :not_started, :imaging_uploaded, :virtual_model_ready, :after_blood_flow_simulation ]
 
   has_many :data_files, dependent: :destroy
+  has_many :computations, dependent: :destroy
   has_one :computation
 
   validates :case_number, :procedure_status, presence: true
