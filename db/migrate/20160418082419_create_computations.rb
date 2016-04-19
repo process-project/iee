908eae1bf58aa6c77a7a383c11202dd0ca0c13ce
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-04-19 14:37:34 +0200

    rename nil to null when column cannot be empty

--- db/migrate/20160418082419_create_computations.rb
+++ db/migrate/20160418082419_create_computations.rb
@@ -2,9 +2,9 @@ class CreateComputations < ActiveRecord::Migration
   def change
     create_table :computations do |t|
       t.string :job_id
-      t.text :script, nil: false
+      t.text :script, null: false
       t.string :working_directory, unique: true
-      t.string :status, nil: false, default: 'new'
+      t.string :status, null: false, default: 'new'
 
       t.string :stdout_path
       t.string :stderr_path
