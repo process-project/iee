7ca013f03779e6f54db5cfda119093b6eb99ca9a
tomek <tomek.bartynski@gmail.com>
2016-04-19 13:27:27 +0200

    added a helper for getting a Computation object in views
    https://gitlab.com/eurvalve/vapor/issues/40

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -3,4 +3,8 @@ module PatientsHelper
     status_number = Patient.procedure_statuses[patient.procedure_status] || 0
     "#{status_number.to_f / (Patient.procedure_statuses.size - 1).to_f * 100}%"
   end
+
+  def computation
+    @computation ||= Computation.new
+  end
 end
