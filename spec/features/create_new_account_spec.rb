1bf34d5698f6033c45cf21006a7d61e8d518bfda
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-05-04 11:19:03 +0200

    Make sure that email is not sent when user is not created

--- spec/features/create_new_account_spec.rb
+++ spec/features/create_new_account_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature 'User registration' do
-  scenario 'email is sent to supervisors after new user registered' do
+  before do
     supervisor_group = create(:group, name: 'supervisor')
     create(:user, groups: [supervisor_group])
+  end
 
+  scenario 'email is sent to supervisors after new user registered' do
     visit new_user_registration_path
+
     fill_in 'user_first_name', with: 'John'
     fill_in 'user_last_name', with: 'Doe'
     fill_in 'user_email', with: 'john@doe'
     fill_in 'user_password', with: 'verysecretpass'
     fill_in 'user_password_confirmation', with: 'verysecretpass'
 
-    expect{
-      click_on 'Register'
-      # puts ">>>>> #{page.body}"
-    }.
+    expect{ click_on 'Register' }.
       to change { ActionMailer::Base.deliveries.count }.by(1)
+  end
+
+  scenario 'email is not sent when user is not created' do
+    visit new_user_registration_path
+
+    expect{ click_on 'Register' }.
+      to_not change { ActionMailer::Base.deliveries.count }
   end
 end
