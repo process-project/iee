4e764456cc2eaf18478c68942ecb6f597f661a22
tomek <tomek.bartynski@gmail.com>
2016-06-01 09:10:12 +0200

    add self association in Group model
    https://gitlab.com/eurvalve/vapor/issues/59
    
    Two version of a method that gets all parent groups. Those methods will be benchamarked and reviewed and the better one will be selected.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,20 @@ class Group < ActiveRecord::Base
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :permissions, dependent: :destroy
+  has_many :subgroups, class_name: 'Group', foreign_key: 'parent_group'
+  belongs_to :parent_group, class_name: 'Group'
 
   validates :name, presence: true
+
+  def all_parents
+    if parent_group
+      [parent_group] + parent_group.all_parents
+    else
+      []
+    end
+  end
+
+  def all_parents_comp
+    ([parent_group] + [parent_group.try(:parent_group)]).flatten.compact!
+  end
 end
