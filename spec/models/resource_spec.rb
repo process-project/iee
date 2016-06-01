72d309f424ebacde0a3e49ca361102d2668a0c22
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-06-01 12:03:53 +0200

    closes #64: resources are found by regular expression comparison
    This only relates to queries within the policy decision point
    implementation.

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Resource do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:uri) }
   it { should have_many(:permissions).dependent(:destroy) }
+  
+  it "checks regular expression match" do
+    resource = build(:resource, uri: "http://host/.*")
+    
+    resource.save
+    
+    expect(Resource.where(":uri ~ uri", uri: "http://host/something").count).
+      to equal(1)
+  end
+  
+  it "still should pick exact matches correctly" do
+    resource = build(:resource, uri: "http://host/exact")
+    
+    resource.save
+    
+    expect(Resource.where(":uri ~ uri", uri: "http://host/exact").count).
+      to equal(1)
+  end
 end
