17559dbc6fe19d8de823c412ff3f66f33f751a16
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-06-06 11:24:26 +0200

    closes #63: additional filtering applied to user's permissions

--- spec/features/resource_permission_ui_spec.rb
+++ spec/features/resource_permission_ui_spec.rb
@@ -42,4 +42,34 @@ RSpec.feature "Resource and permissions UI management" do
     expect(page).not_to(have_content(resource_name))
     expect(page).not_to(have_content(resource_uri))
   end
+  
+  scenario "permissions for different resources should not mix" do
+    #user creates two resources
+    user1 = create(:approved_user)
+    create(:action, name: "manage")
+    resource_name = "My New Resource"
+    resource_uri = "http://host.com/my_resource"
+    resource_name_2 = "My New Resource 2"
+    resource_uri_2 = "http://host.com/my_resource_2"
+    
+    #creating the first resource
+    sign_in_as(user1)
+    visit(new_resource_path)
+    fill_in(:resource_name, with: resource_name)
+    fill_in(:resource_uri, with: resource_uri)
+    find("form#new_resource").find("input.btn").click
+    
+    #creating the second resource
+    visit(new_resource_path)
+    fill_in(:resource_name, with: resource_name_2)
+    fill_in(:resource_uri, with: resource_uri_2)
+    find("form#new_resource").find("input.btn").click
+    
+    #showing the permission management tab for the first resource
+    resource_1 = Resource.find_by(uri: resource_uri)
+    visit(new_permission_path(resource_id: resource_1.id))
+    
+    #the page should contain two manage texts: one for the resource and the second one for the menu
+    expect(page).to(have_content("manage", count: 2))
+  end
 end
