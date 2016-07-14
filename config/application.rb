8f9ed22aee3046f869cd50000a0964d2ff71ab06
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-07-14 12:32:26 +0200

    After rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-#require "rails/test_unit/railtie" #rspec used instead
+require 'rails/all'
 
 require File.expand_path('lib/jwt/config')
 
@@ -23,22 +14,8 @@ module Vapor
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
     # Custom error pages
     config.exceptions_app = routes
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-    # Separate controller helpers to avoid conflicts
-    config.action_controller.include_all_helpers = false
 
     config.constants = config_for(:application)
 
