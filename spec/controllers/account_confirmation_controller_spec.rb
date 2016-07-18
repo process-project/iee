76f08cce36fc248e309801b0b1597851dc781c88
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-07-18 10:52:13 +0200

    Use integration test request method keywords
    This commit fixes following error: DEPRECATION WARNING:
    ActionDispatch::IntegrationTest HTTP request methods will accept only
    the following keyword arguments in future Rails versions: params,
    headers, env, xhr, as

--- spec/controllers/account_confirmation_controller_spec.rb
+++ spec/controllers/account_confirmation_controller_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe AccountConfirmationController, type: :controller do
   it "should not allow a supervisor to block himself" do
     user = create(:supervisor_user)
     sign_in(user)
-    
-    put :block, id: user.id
-    
+
+    put :block,
+        params: { id: user.id }
+
     expect(response).to redirect_to(account_confirmations_index_path)
     expect(flash[:alert]).to eq(I18n.t("cannot_block_itself"))
   end
