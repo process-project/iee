a4b6d52df94049ebf90ac41c17d53b15860d0cf5
Tomasz Gubala <t.gubala@cyfronet.pl>
2016-07-18 12:50:02 +0200

    r4.2 -> r5.0: moves account confirmation spec from controller to request

---
+++ spec/requests/account_confirmations_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'AccountConfirmations' do
+  context 'with user signed in' do
+    let(:user) { create(:supervisor_user) }
+    before { login_as(user) }
+
+    describe 'DELETE /account_confirmations/:id' do
+      it 'should not allow a supervisor to block himself' do
+        expect {
+          delete '/account_confirmations/',
+                 params: { id: user.id }
+        }.not_to change { user.approved }
+        expect(user.approved).to be_truthy
+
+        expect(response).to redirect_to(account_confirmations_index_path)
+        follow_redirect!
+        expect(flash[:alert]).to eq(I18n.t("cannot_block_itself"))
+      end
+    end
+  end
+end
