03451c30dcda67b8d5368bbd289f6a25b5ab645b
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-07-19 08:46:47 +0200

    Move access policies controller tests into request tests
    Controller tests are deprecated in rails 5.0.

--- spec/requests/access_policies_spec.rb
+++ spec/requests/access_policies_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe AccessPoliciesController, type: :controller do
-  render_views
-
+RSpec.describe 'Access Policies' do
   before do
     @user = create(:approved_user)
     @resource = create(:resource)
     @access_method = create(:access_method)
-    sign_in(@user)
+    login_as(@user)
   end
 
   it "should return an error message when neither user_id or group_id is chosen" do
-    post :create, params: access_policy_params("")
+    post access_policies_path, params: access_policy_params("")
 
     expect(response).to render_template(:new)
     expect(response.body).to include(I18n.t("either_user_or_group"))
   end
 
   it "should return an error message when no access method was chosen" do
-    post :create,
+    post access_policies_path,
          params: {
            access_policy: {
              user_id: "",
@@ -32,19 +30,18 @@ RSpec.describe AccessPoliciesController, type: :controller do
   end
 
   it "should add a new access policy to the database" do
-    post :create, params: access_policy_params(@user.id)
+    post access_policies_path,
+         params: access_policy_params(@user.id)
 
     expect(response).to redirect_to(new_access_policy_path(resource_id: @resource.id))
   end
 
   it "should create only single access policy for a given method" do
-    post :create, params: access_policy_params(@user.id)
-    post :create, params: access_policy_params(@user.id)
-    post :create, params: access_policy_params(@user.id)
-
-    get :new, params: { resource_id: @resource.id }
-
-    expect(response.body).to have_selector("span.label:contains('#{@access_method.name}')", count: 1)
+    expect do
+      post access_policies_path, params: access_policy_params(@user.id)
+      post access_policies_path, params: access_policy_params(@user.id)
+      post access_policies_path, params: access_policy_params(@user.id)
+    end.to change{ AccessPolicy.count }.by(1)
   end
 
   def access_policy_params(user_id)
