14a93fdc0d226afeedbc3ebe423c8c06ec49d712
tomek <tomek.bartynski@gmail.com>
2016-08-02 13:31:06 +0200

    reduced ABC metric in method getting access policies
    https://gitlab.com/eurvalve/vapor/issues/80

--- app/policies/resource_policy.rb
+++ app/policies/resource_policy.rb
@@ -33,9 +33,9 @@ class ResourcePolicy < ApplicationPolicy
     AccessPolicy.joins(:access_method).
       includes(:group).references(:group).
       where(
-        'access_policies.user_id = :user_id OR groups.id IN (:group_ids)',
-        user_id: user.id, group_ids: group_ids
-      ).where(resource_id: record.id).
-      where('LOWER(access_methods.name) = :name', name: access_method_name.downcase)
+        '(access_policies.user_id = :user_id OR groups.id IN (:group_ids))'\
+        ' AND resource_id = :resource_id',
+        user_id: user.id, group_ids: group_ids, resource_id: record.id
+      ).where('LOWER(access_methods.name) = :name', name: access_method_name.downcase)
   end
 end
