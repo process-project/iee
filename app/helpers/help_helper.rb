b951f6f29cdfe33d8f8f4b8e9c3687f2e019bf42
tomek <tomek.bartynski@gmail.com>
2016-08-02 13:44:35 +0200

    shortened too long method
    https://gitlab.com/eurvalve/vapor/issues/80

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -3,19 +3,15 @@ require 'markdown_renderer'
 
 module HelpHelper
   def markdown(text)
-    unless @markdown_renderer
-      @markdown_renderer =
-        # see https://github.com/vmg/redcarpet#and-its-like-really-simple-to-use
-        Redcarpet::Markdown.new(renderer,
-                                no_intra_emphasis: true,
-                                tables: true,
-                                fenced_code_blocks: true,
-                                autolink: true,
-                                strikethrough: true,
-                                lax_html_blocks: true,
-                                space_after_headers: true,
-                                superscript: true)
-    end
+    @markdown_renderer ||=
+      # see https://github.com/vmg/redcarpet#and-its-like-really-simple-to-use
+      Redcarpet::Markdown.new(renderer,
+                              no_intra_emphasis: true, tables: true,
+                              fenced_code_blocks: true,
+                              autolink: true, strikethrough: true,
+                              lax_html_blocks: true,
+                              space_after_headers: true,
+                              superscript: true)
 
     @markdown_renderer.render(text || '').html_safe
   end
