68c385b81bade4ca635cb8bc4edd85a6c95ac09f
tomek <tomek.bartynski@gmail.com>
2016-08-02 12:37:03 +0200

    stopped using obsolete controller spec helper
    https://gitlab.com/eurvalve/vapor/issues/80

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'support/helpers/controller_helper.rb'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -55,8 +54,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
 
   config.filter_run_excluding proxy: true
