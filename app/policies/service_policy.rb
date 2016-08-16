4ae1d58cd7cf2f4f7f342e313174ad11d9db40f6
Tomasz Gubala <t.gubala@cyfronet.pl>
2016-08-16 16:52:11 +0200

    index, new, create and delete service actions

--- app/policies/service_policy.rb
+++ app/policies/service_policy.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 class ServicePolicy < ApplicationPolicy
-  def update?
+  class Scope < ApplicationPolicy::ApplicationScope
+    def resolve
+      scope.
+        joins(:users).
+        where(users: { id: user.id })
+    end
+  end
+
+  def update?; owned? end
+  def destroy?; owned? end
+
+  private
+
+  def owned?
     record.users.include? user
   end
 end
