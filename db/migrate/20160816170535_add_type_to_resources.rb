a902f4fabc538e5dc0e7ee23a27ea745166277c6
Daniel Harezlak <d.harezlak@cyfronet.pl>
2016-08-17 07:00:53 +0200

    #77: migrations merged

--- db/migrate/20160816170535_add_type_to_resources.rb
+++ db/migrate/20160816170535_add_type_to_resources.rb
@@ -1,5 +1,16 @@
 class AddTypeToResources < ActiveRecord::Migration[5.0]
   def change
     add_column :resources, :resource_type, :integer, default: 0, null: false
+    
+    reversible do |dir|
+      dir.up do
+        Resource.all.each do |resource|
+          resource.path.start_with?('webdav') ? resource.global! : resource.local!
+        end
+      end
+      dir.down do
+        #not used
+      end
+    end
   end
 end
