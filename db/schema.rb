4e6e7415558ba2f0fd4986e5f07a9bbb093003e7
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-08-24 23:07:15 +0200

    Default boolean field added to group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822072902) do
+ActiveRecord::Schema.define(version: 20160824155823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,9 @@ ActiveRecord::Schema.define(version: 20160822072902) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "default",    default: false, null: false
   end
 
   create_table "patients", force: :cascade do |t|
