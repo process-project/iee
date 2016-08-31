c99361c77daee52017f300bcd452b66d78fb6214
Jan Meizner <jan.meizner@cyfronet.pl>
2016-08-31 11:32:39 +0200

    - fixed test for uniqueness (default is not working for the PSQL array)

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe Service do
 
   it { should validate_presence_of(:uri) }
   it { should validate_uniqueness_of(:uri) }
-  it { should validate_uniqueness_of(:uri_aliases) }
+
+  # NOTE: default uniqueness test seams to not work properly for PSQL Tables
+  it 'validates uri_aliases uniqueness' do
+    s1 = create(:service)
+    service = build(:service, uri_aliases: s1.uri_aliases)
+
+    expect(service).to_not be_valid
+  end
 
   it { should have_many(:resources).dependent(:destroy) }
 
