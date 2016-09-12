6de00467863e888c5b77bcc6cc08a17da0f5204b
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-09-12 10:28:12 +0200

    Show service token to service owner

---
+++ spec/features/service_token_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature 'Service secret token' do
+  let(:user) { create(:approved_user) }
+
+  before do
+    login_as(user)
+  end
+
+  scenario 'is not visible for non service owner' do
+    service = create(:service)
+
+    visit service_path(service)
+
+    expect(page).to_not have_content(service.token)
+  end
+
+  scenario 'is visible for service owner' do
+    service = create(:service, users: [user])
+
+    visit service_path(service)
+
+    expect(page).to have_content(service.token)
+  end
+end
