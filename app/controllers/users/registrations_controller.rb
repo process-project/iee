1ca41af26d905d7ac1d43ff8601b3c7d30fa9d4d
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-09-19 08:41:57 +0200

    Improved notice after new user account created.
    `signed_up_but_not_approved` method was overridden to not hit into `/`
    path which is secured (and as a conclusion user is redirected into sign
    in path where we lose notice msg.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,5 +7,11 @@ module Users
         Users::AddToDefaultGroups.new(resource).call
       end
     end
+
+    protected
+
+    def after_inactive_sign_up_path_for(_resource)
+      new_user_session_path
+    end
   end
 end
