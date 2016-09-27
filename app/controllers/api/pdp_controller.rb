33971ed9ae8ef617be02e3821f9919fc81cb134d
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-09-27 11:37:12 +0200

    Return 403 when mandatory pdp qury params are missing
    Mandatory parameters are: `uri` and `access_method`

--- app/controllers/api/pdp_controller.rb
+++ app/controllers/api/pdp_controller.rb
@@ -10,9 +10,11 @@ module Api
     # policies of all matching resources which include the access method in question
     # must allow for access and at least one policy has to exist
     def permit?
+      return false if access_method.blank? || uri.blank?
+
       resources = service&.resources&.joins(access_policies: :access_method)&.
         where(':path ~ path', path: path)&.
-        where(access_methods: { name: params[:access_method].downcase })
+        where(access_methods: { name: access_method })
       every_resource_permitted? resources
     end
 
@@ -28,11 +30,15 @@ module Api
     end
 
     def every_resource_permitted?(resources)
-      resources&.map { |r| policy(r).permit?(params[:access_method]) }&.reduce(:&)
+      resources&.map { |r| policy(r).permit?(access_method) }&.reduce(:&)
     end
 
     def uri
       params[:uri]
+    end
+
+    def access_method
+      params[:access_method]&.downcase
     end
   end
 end
