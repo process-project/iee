3d8671d6f6686f4c5847667a0acee93900192aa6
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-09-28 06:52:52 +0200

    Remove overused `&.`
    By checking if service exists we can remove a lot of `&.` constructions.

--- app/controllers/api/pdp_controller.rb
+++ app/controllers/api/pdp_controller.rb
@@ -10,12 +10,13 @@ module Api
     # policies of all matching resources which include the access method in question
     # must allow for access and at least one policy has to exist
     def permit?
-      return false if access_method.blank? || uri.blank?
+      return false if access_method.blank? || uri.blank? || service.nil?
 
-      resources = service&.resources&.joins(access_policies: :access_method)&.
-        where(':path ~ path', path: path)&.
-        where(access_methods: { name: access_method })
-      every_resource_permitted? resources
+      resources = service.resources.
+                  joins(access_policies: :access_method).
+                  where(':path ~ path', path: path).
+                  where(access_methods: { name: access_method })
+      every_resource_permitted?(resources)
     end
 
     def path
@@ -24,13 +25,17 @@ module Api
     end
 
     def service
+      @service ||= find_service
+    end
+
+    def find_service
       Service.find_each do |service|
         return service if ([service.uri] + service.uri_aliases).any? { |u| uri.starts_with?(u) }
       end
     end
 
     def every_resource_permitted?(resources)
-      resources&.map { |r| policy(r).permit?(access_method) }&.reduce(:&)
+      resources.map { |r| policy(r).permit?(access_method) }.reduce(:&)
     end
 
     def uri
