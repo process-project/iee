a827def7f9351cba3ddaa5bea4e5f4b8dfc1f760
Marek Kasztelnik <mkasztelnik@gmail.com>
2017-01-24 14:45:15 +0100

    Limit number of sql queries

--- spec/requests/api/policy_entities_spec.rb
+++ spec/requests/api/policy_entities_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe 'Policy entities API' do
   it 'should return a JSON object with policy entities' do
     create(:service, uri: 'https://service.host.com', token: 'random_token')
     group = create(:group, name: 'group_name')
+    user = group.users.first
     access_method = create(:access_method, name: 'get')
     auth_headers = {
       'X-SERVICE-TOKEN' => 'random_token',
-      'Authorization' => "Bearer #{group.users.first.token}"
+      'Authorization' => "Bearer #{user.token}"
     }
 
     get api_policy_entities_path, headers: auth_headers
@@ -19,7 +20,7 @@ RSpec.describe 'Policy entities API' do
     expect(response.status).to eq(200)
     expect(response_json).to include_json(
       policy_entities: [
-        { type: 'user_entity', name: group.users.first.email },
+        { type: 'user_entity', name: user.email },
         { type: 'group_entity', name: group.name },
         { type: 'access_method_entity', name: access_method.name }
       ]
