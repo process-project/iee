2560cada6bc132ae46fea9d3c9d55d88b3c92768
tomek <tomek.bartynski@gmail.com>
2017-03-13 10:16:55 +0100

    prevent from using base script generator
    https://gitlab.com/eurvalve/vapor/issues/187
    
    ComputationScriptGenerator is a base class that does not know what script it should generate
    so by default it raises and error unless methods responsible for script generation are dynamically overriden.
    In most cases one should use specializations of this class.

--- app/services/computation_script_generator.rb
+++ app/services/computation_script_generator.rb
@@ -1,15 +1,36 @@
 # frozen_string_literal: true
 class ComputationScriptGenerator
-  def initialize(patient, user)
-    @patient = patient
-    @user = user
-  end
 
   def script
     header + stage_in + job_script + stage_out
   end
 
+  def initialize(patient, user)
+    @patient = patient
+    @user = user
+  end
+
   private
+
+  def header
+    invalid_usage
+  end
+
+  def invalid_usage
+    raise 'Method called on abstract base class. Use specializations of this class instead.'
+  end
+
+  def stage_in
+    invalid_usage
+  end
+
+  def job_script
+    invalid_usage
+  end
+
+  def stage_out
+    invalid_usage
+  end
 
   def grant_id
     Rails.application.config_for('eurvalve')['grant_id']
