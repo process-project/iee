c62f1846f134bfa9dabe715793644d9b5db9e5ad
tomek <tomek.bartynski@gmail.com>
2017-03-15 09:56:24 +0100

    updated factory to include pipeline step
    https://gitlab.com/eurvalve/vapor/issues/187

--- spec/factories/computations.rb
+++ spec/factories/computations.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :computation do
+    pipeline_step { Patient::PIPELINE.keys.first.to_s }
     script { Faker::Lorem.sentence }
     working_directory { Faker::Lorem.characters(10) }
 
