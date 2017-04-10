69fbddae4b94d1deb6f2c63523a467f04ebb8db8
Daniel Harezlak <d.harezlak@cyfronet.pl>
2017-04-10 16:13:56 +0200

    #161: subresource manage check added

--- app/controllers/api/policies_controller.rb
+++ app/controllers/api/policies_controller.rb
@@ -24,7 +24,7 @@ module Api
         end
       else
         if copy_or_move_request?
-          if ResourcePolicy.user_owns_resources?(current_user, [PathService.to_path(copy_move_path)])
+          if ResourcePolicy.user_owns_resources?(current_user, subresource_paths(PathService.to_path(copy_move_path)))
             Policies::CopyMovePolicy.new(@json, service, current_user).call
 
             head :created
@@ -97,6 +97,12 @@ module Api
 
     def copy_move_path
       @json['copy_from'].presence || @json['move_from']
+    end
+
+    def subresource_paths(path)
+      Resource.where('path like :prefix', prefix: "^#{path}%").map do |resource|
+        resource.path
+      end
     end
   end
 end
