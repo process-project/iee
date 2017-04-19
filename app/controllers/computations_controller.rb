b3f591cf07b6f30b18aa24fe7c5e8d2f8544e070
tomek <tomek.bartynski@gmail.com>
2017-04-19 09:24:38 +0200

    enhanced building computation and req params validation
    https://gitlab.com/eurvalve/vapor/issues/187

--- app/controllers/computations_controller.rb
+++ app/controllers/computations_controller.rb
@@ -9,17 +9,21 @@ class ComputationsController < ApplicationController
   end
 
   def create
-    @computation = Patient::PIPELINE[patient.procedure_status.to_sym].run(patient, current_user)
+    @computation = computation_builder_for(patient.procedure_status).run(patient, current_user)
     redirect_to @computation.patient, notice: 'Computation submitted'
   end
 
   private
+
+  def computation_builder_for(procedure_status)
+    Patient::PIPELINE[procedure_status.to_sym]
+  end
 
   def create_params
     params.require(:computation).permit(:patient_id)
   end
 
   def patient
-    @patient ||= Patient.find(params[:computation][:patient_id])
+    @patient ||= Patient.find(create_params[:patient_id])
   end
 end
