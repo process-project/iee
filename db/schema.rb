b97f31dbb6beeb45f9fa3c17026c933df634897a
tomek <tomek.bartynski@gmail.com>
2017-05-15 15:38:33 +0200

    allowed null value in script field in DB
    https://gitlab.com/eurvalve/vapor/issues/177

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313131252) do
+ActiveRecord::Schema.define(version: 20170515074920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20170313131252) do
 
   create_table "computations", force: :cascade do |t|
     t.string   "job_id"
-    t.text     "script",                            null: false
+    t.text     "script"
     t.string   "working_directory"
     t.string   "status",            default: "new", null: false
     t.string   "stdout_path"
