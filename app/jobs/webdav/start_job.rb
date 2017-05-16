dc6408e20d33ab3de4ff2eba7a7447165a0df061
tomek <tomek.bartynski@gmail.com>
2017-05-16 16:52:20 +0200

    set status of webdav-based computations
    https://gitlab.com/eurvalve/vapor/issues/177

--- app/jobs/webdav/start_job.rb
+++ app/jobs/webdav/start_job.rb
@@ -5,6 +5,10 @@ module Webdav
 
     def perform(computation)
       Segmentation::Start.new(computation).call
+      computation.update_attributes(status: 'running')
+    rescue
+      computation.update_attributes(status: 'error')
+      raise $ERROR_INFO
     end
   end
 end
