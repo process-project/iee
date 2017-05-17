31aded902987037f432b4eec87a422b3521860cb
tomek <tomek.bartynski@gmail.com>
2017-05-17 15:58:10 +0200

    enhanced readability of start job
    https://gitlab.com/eurvalve/vapor/issues/177

--- app/services/segmentation/start.rb
+++ app/services/segmentation/start.rb
@@ -2,13 +2,30 @@
 
 module Segmentation
   class Start
+    include FileUtils
     def initialize(computation)
+      @computation = computation
       @download_input = DownloadInput.new(computation.input_path, computation.user.token)
     end
 
     def call
-      local_path = @download_input.call
-      UploadInput.new(local_path).call
+      download_input
+      update_computation
+      upload_input
+    end
+
+    private
+
+    def download_input
+      @local_path = @download_input.call
+    end
+
+    def update_computation
+      @computation.update_attributes(working_file_name: strip_local_filename(@local_path))
+    end
+
+    def upload_input
+      UploadInput.new(@local_path).call
     end
   end
 end
