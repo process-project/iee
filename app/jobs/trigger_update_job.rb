3855f7236f226825a79e5978350c0929c734ff13
tomek <tomek.bartynski@gmail.com>
2017-05-23 11:57:09 +0200

    distinguish between Rimrock and Webdav-based while triggering job update
    https://gitlab.com/eurvalve/vapor/issues/177

--- app/jobs/trigger_update_job.rb
+++ app/jobs/trigger_update_job.rb
@@ -4,8 +4,21 @@ class TriggerUpdateJob < ApplicationJob
   queue_as :computation
 
   def perform
-    User.with_submitted_computations.each do |user|
+    trigger_rimrock_jobs_update
+    trigger_webdav_jobs_update
+  end
+
+  private
+
+  def trigger_rimrock_jobs_update
+    User.with_submitted_computations('RimrockComputation').each do |user|
       Rimrock::UpdateJob.perform_later(user)
+    end
+  end
+
+  def trigger_webdav_jobs_update
+    User.with_submitted_computations('WebdavComputation').each do |user|
+      Webdav::UpdateJob.perform_later(user)
     end
   end
 end
--- spec/jobs/rimrock/trigger_update_job_spec.rb
+++ spec/jobs/rimrock/trigger_update_job_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe Rimrock::TriggerUpdateJob do
   it 'triggers update for all users with active jobs' do
     u1, u2 = create_list(:user, 2)
 
-    create(:computation, status: 'running', user: u1)
-    create(:computation, status: 'finished', user: u2)
+    create(:rimrock_computation, status: 'running', user: u1)
+    create(:webdav_computation, status: 'new', user: u1)
+    create(:rimrock_computation, status: 'finished', user: u2)
+    create(:webdav_computation, status: 'running', user: u2)
 
     expect(Rimrock::UpdateJob).to receive(:perform_later).with(u1)
     expect(Rimrock::UpdateJob).to_not receive(:perform_later).with(u2)
+    expect(Webdav::UpdateJob).to receive(:perform_later).with(u2)
+    expect(Webdav::UpdateJob).to_not receive(:perform_later).with(u1)
 
     described_class.perform_now
   end
