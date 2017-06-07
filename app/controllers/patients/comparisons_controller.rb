452fdb2ae00a3dcff76b74cd8c0278c7613ca666
Tomasz Gubala <t.gubala@cyfronet.pl>
2017-06-07 17:28:27 +0200

    better comparison data preparation

--- app/controllers/patients/comparisons_controller.rb
+++ app/controllers/patients/comparisons_controller.rb
@@ -8,13 +8,33 @@ module Patients
         redirect_to patient_path(@patient),
                     alert: I18n.t('patients.comparisons.show.invalid')
       end
+
       @patient.execute_data_sync(current_user)
+
+      @data = []
+      pipelines.first.data_files.each do |compared|
+        compare_to = pipelines.second.data_files.detect { |df| df.data_type == compared.data_type }
+        if compare_to.present?
+          @data << { data_type: t("data_file.data_types.#{compared.data_type}"),
+                     compared: {
+                       name: compared.name,
+                       content: compared.content(current_user),
+                       pipeline: compared.pipeline.name
+                     },
+                     compare_to: {
+                       name: compare_to.name,
+                       content: compare_to.content(current_user),
+                       pipeline: compare_to.pipeline.name
+                     }
+          }
+        end
+      end
     end
 
     private
 
     def pipelines
-      @pipelines ||= patient.pipelines.where(iid: params[:pipeline_ids])
+      @pipelines ||= patient.pipelines.where(iid: params[:pipeline_ids]).includes(:data_files)
     end
 
     def patient
--- app/models/pipeline.rb
+++ app/models/pipeline.rb
@@ -16,44 +16,6 @@ class Pipeline < ApplicationRecord
     File.join(patient.pipelines_dir, iid.to_s, '/')
   end
 
-  # NOTE: a temporary method to represent DataFiles stored for a given pipeline
-  # Future version will remove this in favor of a has_many data_files relation
-  # and synchronisation with WebDAV.
-  include SynchronizerUtilities
-  def data_files
-    # dav_client = WebdavClient.new(
-    #   webdav_storage_url,
-    #   headers: {
-    #     'Authorization' => "Bearer #{user.try(:token)}"
-    #   }
-    # )
-    #
-    # remote_names = []
-    # @patient = patient
-    # pipeline_directory = "#{case_directory(webdav_storage_url)}/pipelines/#{iid}"
-    # dav_client.find(pipeline_directory, recursive: false) do |item|
-    #   remote_names << item.properties.displayname
-    # end
-    #
-    # remote_names.map do |remote_name|
-    #   data_type = recognize_data_type(remote_name)
-    #   next unless data_type
-    #   {
-    #     name: remote_name,
-    #     data_type: data_type,
-    #     content: dav_client.get_file_to_memory("#{pipeline_directory}/#{remote_name}")
-    #   }
-    # end
-    []
-  end
-
-  # def get_file_to_memory(remote_path)
-  #   io = StringIO.new
-  #   @dav_client.get(remote_path) { |s| io.write(s) }
-  #   io.close
-  #   io.string
-  # end
-
   private
 
   def set_iid
