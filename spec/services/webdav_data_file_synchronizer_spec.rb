189afb4bee6f9465c86e4930d8ad89751b4cc0e2
Tomasz Gubala <t.gubala@cyfronet.pl>
2017-06-07 13:40:49 +0200

    WIP: pipelines has data files; initial diff work

--- spec/services/webdav_data_file_synchronizer_spec.rb
+++ spec/services/webdav_data_file_synchronizer_spec.rb
@@ -109,29 +109,23 @@ describe WebdavDataFileSynchronizer, files: true do
         expect(DataFile.first.pipeline).to eq test_patient_with_pipeline.pipelines.first
       end
 
-      it 'only creates data_files not yet present' do
-        create(:data_file, name: 'structural_vent.dat',
-               data_type: 'ventricle_virtual_model',
-               patient: test_patient)
-        expect { call(test_patient, correct_user) }.to change { DataFile.count }.by(1)
-        expect(DataFile.all.map(&:data_type)).
-            to match_array %w(fluid_virtual_model ventricle_virtual_model)
+      it 'only creates pipeline data_files not yet present' do
+        create(:data_file, name: 'fluidFlow-1-00002.dat',
+               data_type: 'blood_flow_result',
+               patient: test_patient_with_pipeline,
+               pipeline: test_patient_with_pipeline.pipelines.first)
+        expect { call(test_patient_with_pipeline, correct_user) }.to change { DataFile.count }.by(0)
       end
 
-      it 'destroys data_files which are no longer stored in File Storage' do
-        create(:data_file, data_type: 'blood_flow_result', patient: test_patient)
-        create(:data_file, data_type: 'blood_flow_model', patient: test_patient)
+      it 'destroys pipeline data_files which are no longer stored in File Storage' do
         create(:data_file, name: 'structural_vent.dat',
                data_type: 'ventricle_virtual_model',
-               patient: test_patient)
-        create(:data_file, name: 'fluidFlow.cas',
-               data_type: 'fluid_virtual_model',
-               patient: test_patient)
-        expect(test_patient.reload.after_blood_flow_simulation?).to be_truthy
-        expect { call(test_patient, correct_user) }.to change { DataFile.count }.by(-2)
-        expect(DataFile.all.map(&:data_type)).
-            to match_array %w(fluid_virtual_model ventricle_virtual_model)
-        expect(test_patient.reload.virtual_model_ready?).to be_truthy
+               patient: test_patient_with_pipeline,
+               pipeline: test_patient_with_pipeline.pipelines.first)
+        call(test_patient_with_pipeline, correct_user)
+        expect(test_patient_with_pipeline.reload.data_files.count).to eq 1
+        expect(test_patient_with_pipeline.pipelines.first.data_files.count).to eq 1
+        expect(test_patient_with_pipeline.data_files.data_type).to eq 'blood_flow_result'
       end
     end
   end
