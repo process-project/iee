d83dd2e43a188d0f1b64dac5920693203965a32a
Tomasz Gubala <t.gubala@cyfronet.pl>
2017-06-08 14:37:56 +0200

    specs require file store access

--- spec/features/comparing_two_pipelines_spec.rb
+++ spec/features/comparing_two_pipelines_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.feature 'Comparing two pipelines' do
+RSpec.feature 'Comparing two pipelines', files: true do
   let(:patient) { create(:patient, :with_pipeline) }
   let!(:pipelines) { create_list(:pipeline, 2, patient: patient) }
 
@@ -10,7 +10,7 @@ RSpec.feature 'Comparing two pipelines' do
     login_as(user)
   end
 
-  scenario 'shows data file diffs', js: true, files: true do
+  scenario 'shows data file diffs', js: true do
     visit patient_comparison_path(patient, id: patient.id, pipeline_ids: pipelines.map(&:iid))
 
     expect(page).to have_content 'Result: Estimated parameters'
@@ -19,14 +19,14 @@ RSpec.feature 'Comparing two pipelines' do
       to match_array %w(0 WRONG! 0.965 96.5)
   end
 
-  scenario 'hides non-paired and noncomparable files', js: true, files: true do
+  scenario 'hides non-paired and noncomparable files', js: true do
     visit patient_comparison_path(patient, id: patient.id, pipeline_ids: pipelines.map(&:iid))
 
     expect(page).to have_content 'Result: Blood flow model. Not compared.'
     expect(page).to have_css '.diff_output', count: 1
   end
 
-  scenario 'refuses to work for >2 pipelines', js: true do
+  scenario 'refuses to work for >2 pipelines' do
     visit patient_comparison_path(patient, id: patient.id, pipeline_ids: Pipeline.all.map(&:iid))
 
     expect(current_path).to eq patient_path(patient)
