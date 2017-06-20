66f5cc79391b5a1f09aee05c5dc1150021219e30
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2017-06-20 15:07:18 +0200

    Fix problems identified in int. review

--- app/services/webdav/upload_zip_file.rb
+++ app/services/webdav/upload_zip_file.rb
@@ -6,21 +6,19 @@ module Webdav
   class UploadZipFile
     def initialize(dav_client, local_path, remote_directory)
       @dav_client = dav_client
-      @local_path = local_path
-      @remote_directory = remote_directory
+      @source_zip_path = local_path
+      @target_dir = remote_directory
     end
 
     def call
-      # Spawn temporary directory to store extracted files
       Dir.mktmpdir do |tmp_dir|
 
-        Zip::File.open(@local_path) do |zip_file|
+        Zip::File.open(@source_zip_path) do |zip_file|
           zip_file.each do |file|
-            tmp_local_path = File.join(tmp_dir, file.name)
-            zip_file.extract(file, tmp_local_path)
+            local_path = File.join(tmp_dir, file.name)
+            zip_file.extract(file, local_path)
 
-            # Construct remote path for this file
-            @dav_client.put_file(tmp_local_path, File.join(@remote_directory, file.name))
+            @dav_client.put_file(local_path, File.join(@target_dir, file.name))
           end
         end
 
