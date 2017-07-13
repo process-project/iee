f1611287930261e4741eaad17df72c9a98c2dafd
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2017-07-13 16:27:34 +0200

    Add specs for missing repository and wrong user token.

--- spec/services/gitlab/versions_spec.rb
+++ spec/services/gitlab/versions_spec.rb
@@ -8,9 +8,27 @@ describe Gitlab::Versions do
   it 'downloads branches and tags from gitlab', gitlab: true do
     branches_and_tags = Gitlab::Versions.new(project).call
 
-    expect(branches_and_tags[:branches].length).to eq 2
     expect(branches_and_tags[:branches]).to contain_exactly 'some_branch', 'master'
-    expect(branches_and_tags[:tags].length).to eq 1
     expect(branches_and_tags[:tags]).to eq ['some_tag']
+  end
+
+  it 'returns empty list of branches and tags for nonexistent repository', gitlab: true do
+    Gitlab.configure do |config|
+      config.endpoint = 'http://no.such.url.com/api/v4'
+    end
+
+    branches_and_tags = Gitlab::Versions.new(project).call
+    expect(branches_and_tags[:branches]).to eq []
+    expect(branches_and_tags[:tags]).to eq []
+  end
+
+  it 'returns empty list of branches and tags on token validation failure', gitlab: true do
+    Gitlab.configure do |config|
+      config.private_token = 'foo'
+    end
+
+    branches_and_tags = Gitlab::Versions.new(project).call
+    expect(branches_and_tags[:branches]).to eq []
+    expect(branches_and_tags[:tags]).to eq []
   end
 end
