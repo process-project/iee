62fa7128a795cd74c206f2c683756de3dca143b4
Marek Kasztelnik <mkasztelnik@gmail.com>
2017-08-09 13:08:20 +0200

    Define origin policy for production action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,5 +107,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [
+    %r{https://valve.*\.cyfronet\.pl}
+  ]
 end
 # rubocop:enable BlockLength
