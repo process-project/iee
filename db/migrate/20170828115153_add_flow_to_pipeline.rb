bd441055793e0fef75a6f2a416c332aff06e7d8c
Jan Meizner <jan.meizner@cyfronet.pl>
2017-09-01 08:58:16 +0000

    Update 20170828115153_add_flow_to_pipeline.rb - changed array member to fixed string

--- db/migrate/20170828115153_add_flow_to_pipeline.rb
+++ db/migrate/20170828115153_add_flow_to_pipeline.rb
@@ -1,5 +1,5 @@
 class AddFlowToPipeline < ActiveRecord::Migration[5.1]
   def change
-    add_column :pipelines, :flow, :string, default: Pipeline::FLOWS.first, null: false
+    add_column :pipelines, :flow, :string, default: 'full_body_scan', null: false
   end
 end
