10591ebbcceae9f4e1dfd20c3143b3f1722bf731
Marek Kasztelnik <mkasztelnik@gmail.com>
2017-09-18 07:37:09 +0200

    Start runnable rimrock computations only when proxy valid

--- spec/services/pipelines/start_runnable_spec.rb
+++ spec/services/pipelines/start_runnable_spec.rb
@@ -4,40 +4,66 @@ require 'rails_helper'
 
 describe Pipelines::StartRunnable do
   let(:pipeline) { create(:pipeline, mode: :automatic) }
-
-  it 'starts created runnable pipeline step' do
-    create(:rimrock_computation,
-           status: 'created', pipeline_step: 'blood_flow_simulation',
-           pipeline: pipeline)
-
-    runner = double(runnable?: true)
-
-    allow(PipelineStep::BloodFlowSimulation).to receive(:new).and_return(runner)
-    expect(runner).to receive(:run)
-
-    described_class.new(pipeline).call
+  let(:proxy) do
+    instance_double(Proxy).tap do |proxy|
+      allow(Proxy).to receive(:new).and_return(proxy)
+    end
   end
 
-  it 'does not start already started pipeline step' do
-    create(:rimrock_computation,
-           status: 'running', pipeline_step: 'blood_flow_simulation',
-           pipeline: pipeline)
+  context 'with valid user proxy' do
+    before { allow(proxy).to receive(:valid?).and_return(true) }
 
-    expect(PipelineStep::BloodFlowSimulation).to_not receive(:new)
+    it 'starts created runnable pipeline step' do
+      create(:rimrock_computation,
+             status: 'created', pipeline_step: 'blood_flow_simulation',
+             pipeline: pipeline)
 
-    described_class.new(pipeline).call
+      runner = double(runnable?: true)
+
+      allow(PipelineStep::BloodFlowSimulation).to receive(:new).and_return(runner)
+      expect(runner).to receive(:run)
+
+      described_class.new(pipeline).call
+    end
+
+    it 'does not start already started pipeline step' do
+      create(:rimrock_computation,
+             status: 'running', pipeline_step: 'blood_flow_simulation',
+             pipeline: pipeline)
+
+      expect(PipelineStep::BloodFlowSimulation).to_not receive(:new)
+
+      described_class.new(pipeline).call
+    end
+
+    it 'does not start not runnable pipeline step' do
+      create(:rimrock_computation,
+             status: 'created', pipeline_step: 'blood_flow_simulation',
+             pipeline: pipeline)
+
+      runner = double(runnable?: false)
+
+      allow(PipelineStep::BloodFlowSimulation).to receive(:new).and_return(runner)
+      expect(runner).to_not receive(:run)
+
+      described_class.new(pipeline).call
+    end
   end
 
-  it 'does not start not runnable pipeline step' do
-    create(:rimrock_computation,
-           status: 'created', pipeline_step: 'blood_flow_simulation',
-           pipeline: pipeline)
+  context 'with invalid user proxy' do
+    before { allow(proxy).to receive(:valid?).and_return(false) }
 
-    runner = double(runnable?: false)
+    it 'runnable rimrock computations are not started' do
+      create(:rimrock_computation,
+             status: 'created', pipeline_step: 'blood_flow_simulation',
+             pipeline: pipeline)
 
-    allow(PipelineStep::BloodFlowSimulation).to receive(:new).and_return(runner)
-    expect(runner).to_not receive(:run)
+      runner = double(runnable?: true)
 
-    described_class.new(pipeline).call
+      allow(PipelineStep::BloodFlowSimulation).to receive(:new).and_return(runner)
+      expect(runner).to_not receive(:run)
+
+      described_class.new(pipeline).call
+    end
   end
 end
