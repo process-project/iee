575c6da2950693aa59660921231cc3f0510daf3b
Jan Meizner <jan.meizner@cyfronet.pl>
2018-03-19 09:57:19 +0100

    - minor fixes in policies

--- app/policies/user_audit_policy.rb
+++ app/policies/user_audit_policy.rb
@@ -1,7 +1,37 @@
+# frozen_string_literal: true
+
 class UserAuditPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope
-    end
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def index?
+    user.admin? || owned?
+  end
+
+  def show?
+    user.admin? || owned?
+  end
+
+  def edit?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  private
+
+  def owned?
+    record.user == user
   end
 end
--- spec/policies/user_audit_policy_spec.rb
+++ spec/policies/user_audit_policy_spec.rb
@@ -1,28 +1,47 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe UserAuditPolicy do
-
   let(:user) { User.new }
 
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :new?, :create? do
+    it 'grants access to everyone' do
+      expect(subject).to permit(create(:user), build(:user_audit))
+    end
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :index?, :show? do
+    it 'grants access to user_audit owner' do
+      owner = create(:user)
+
+      expect(subject).to permit(owner, build(:user_audit, user: owner))
+    end
+
+    it 'grants access to admin' do
+      expect(subject).to permit(create(:admin), build(:user_audit))
+    end
+
+    it 'denies access for non user_audit owner' do
+      expect(subject).to_not permit(create(:user), build(:user_audit))
+    end
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  permissions :edit?, :update?, :destroy? do
+    it 'grants access to admin' do
+      expect(subject).to permit(create(:admin), build(:user_audit))
+    end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    it 'denies access to user_audit owner' do
+      owner = create(:user)
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+      expect(subject).to_not permit(owner, build(:user_audit, user: owner))
+    end
+
+    it 'denies access for non user_audit owner' do
+      expect(subject).to_not permit(create(:user), build(:user_audit))
+    end
   end
 end
