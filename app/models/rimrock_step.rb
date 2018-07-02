868162ccbcb67054068d3dd3ffb05d4f353ec9ce
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-07-02 12:17:31 +0200

    Computation script repositories removed from `eurvalve.yml`
    Step repository configuration are used instead

--- app/models/rimrock_step.rb
+++ app/models/rimrock_step.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RimrockStep < Step
-  attr_reader :repo, :file
+  attr_reader :repository, :file
 
   def initialize(name, repository, file, required_files = [])
     super(name, required_files)
