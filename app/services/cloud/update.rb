4c222c66d7a4a4ed86bf26019f289dae137b08c3
Piotr Nowakowski <ymnowako@cyf-kr.edu.pl>
2018-07-26 16:06:03 +0200

    Fix additional issues identified in review

--- app/services/cloud/update.rb
+++ app/services/cloud/update.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+
 module Cloud
   class Update
     def initialize(user, options = {})
@@ -8,7 +12,6 @@ module Cloud
       @on_finish_callback = options[:on_finish_callback]
       @updater = options[:updater]
       @atmosphere_url = Rails.configuration.constants['cloud']['atmosphere_url']
-      @client = Cloud::Client.new(@user_token)
     end
 
     def call
@@ -18,7 +21,38 @@ module Cloud
     end
 
     def update_computation(c)
-      new_status = @client.update_computation(c)
+      url, req = create_request(:get, "#{@atmosphere_url}/api/v1/appliances/#{c.appliance_id}")
+      res = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+
+      res_hash = JSON.parse(res.body)
+
+      # Assume finished if appliance no longer exists
+      new_status = 'finished' if res_hash['appliance'].blank?
+
+      new_status = 'active'
+      # Obtain vm ids from body
+      res_hash['appliance']['virtual_machine_ids'].each do |vm|
+        vm_status = query_vm(vm)
+
+        case vm_status
+        when 'build'
+          new_status = 'queued'
+        when 'active'
+          new_status = 'running'
+        when 'shutoff'
+          new_status = 'finished'
+        when 'error'
+          new_status = 'error'
+        end
+
+        next unless %w[finished error].include? new_status
+        # We're done - clean up
+        appliance_set_id = res_hash['appliance']['appliance_set_id']
+        delete_appliance_set(appliance_set_id)
+      end
+
       if new_status != c.status
         c.update_attributes(status: new_status)
         update(c)
@@ -40,5 +74,31 @@ module Cloud
     def update(computation)
       @updater&.new(computation)&.call
     end
+
+    def query_vm(vm)
+      url, req = create_request(:get, "#{@atmosphere_url}/api/v1/virtual_machines/#{vm}")
+      res = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+
+      res_hash = JSON.parse(res.body)
+
+      # Obtain state from body and return
+      res_hash['virtual_machine']['state']
+    end
+
+    def delete_appliance_set(appliance_set_id)
+      url, req = create_request(
+        :delete,
+        "#{@atmosphere_url}/api/v1/appliance_sets/#{appliance_set_id}"
+      )
+      Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+    end
   end
 end
+
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/CyclomaticComplexity
