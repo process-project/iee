1e435f425e3c3c8778286bc7f2071b1c7a7df33f
Jan Kapala <jankapala@gmail.com>
2018-07-27 09:55:14 +0200

    Patients to projects refactoring in browsing_patients.rb

--- spec/features/browsing_patients_spec.rb
+++ spec/features/browsing_patients_spec.rb
@@ -2,15 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Patient browsing' do
-  let(:patient) { create(:patient, case_number: '1234') }
+RSpec.feature 'Project browsing' do
+  let(:project) { create(:project, project_name: '1234') }
   let(:user) { create(:user, :approved) }
 
   before(:each) do
     login_as(user)
 
-    allow_any_instance_of(Patient).to receive(:execute_data_sync)
-    allow_any_instance_of(Patients::Details).
+    allow_any_instance_of(Project).to receive(:execute_data_sync)
+    allow_any_instance_of(Projects::Details).
       to receive(:call).
       and_return(details: [])
   end
@@ -21,7 +21,7 @@ RSpec.feature 'Patient browsing' do
     allow(Webdav::FileStore).to receive(:new).and_return(file_store)
   end
 
-  context 'in the context of the patients list' do
+  context 'in the context of the projects list' do
     let(:stats) do
       {
         status: :ok,
@@ -43,72 +43,72 @@ RSpec.feature 'Patient browsing' do
       }
     end
 
-    scenario 'has left-hand menu provide a link to patients index' do
+    scenario 'has left-hand menu provide a link to projects index' do
       visit root_path
 
-      expect(page).to have_content I18n.t('layouts.left_menu.research.patients')
+      expect(page).to have_content I18n.t('layouts.left_menu.research.projects')
 
-      click_link I18n.t('layouts.left_menu.research.patients')
+      click_link I18n.t('layouts.left_menu.research.projects')
 
-      expect(current_path).to eq patients_path
+      expect(current_path).to eq projects_path
     end
 
-    scenario 'allows to add a new patient' do
-      visit patients_path
+    scenario 'allows to add a new project' do
+      visit projects_path
 
-      expect(page).to have_content I18n.t('patients.index.add')
+      expect(page).to have_content I18n.t('projects.index.add')
 
-      click_link I18n.t('patients.index.add')
+      click_link I18n.t('projects.index.add')
 
-      expect(current_path).to eq new_patient_path
+      expect(current_path).to eq new_project_path
     end
 
-    scenario 'shows notification when no patients are available' do
-      visit patients_path
+    scenario 'shows notification when no projects are available' do
+      visit projects_path
 
-      expect(page).to have_content I18n.t('patients.index.nothing')
+      expect(page).to have_content I18n.t('projects.index.nothing')
     end
 
-    scenario 'gives the file number for each patient case' do
-      create_list(:data_file, 2, patient: patient)
+    scenario 'gives the file number for each project' do
+      create_list(:data_file, 2, project: project)
 
-      visit patients_path
+      visit projects_path
 
-      expect(page).to have_content "#{I18n.t 'patients.index.files'}: 2"
+      expect(page).to have_content "#{I18n.t 'projects.index.files'}: 2"
     end
 
-    scenario 'gives the pipeline number for each patient case' do
-      create(:pipeline, patient: patient)
+    scenario 'gives the pipeline number for each project' do
+      create(:pipeline, project: project)
 
-      visit patients_path
+      visit projects_path
 
-      expect(page).to have_content "#{I18n.t 'patients.index.pipelines'}: 1"
+      expect(page).to have_content "#{I18n.t 'projects.index.pipelines'}: 1"
     end
 
-    scenario 'lets navigate to a given patient case' do
-      patient
+    scenario 'lets navigate to a given project' do
+      project
 
-      visit patients_path
+      visit projects_path
 
-      expect(page).to have_content(patient.case_number)
+      expect(page).to have_content(project.project_name)
 
-      click_link patient.case_number
+      click_link project.project_name
 
-      expect(current_path).to eq patient_path(patient)
+      expect(current_path).to eq project_path(project)
     end
 
     scenario 'does not fret when there are no pipelines' do
-      patient
+      project
 
-      visit patients_path
+      visit projects_path
 
-      expect(page).to have_content I18n.t('patients.index.no_pipelines')
+      expect(page).to have_content I18n.t('projects.index.no_pipelines')
     end
 
     scenario 'lists pipeline steps in correct flow order' do
-      create(:pipeline, :with_computations, patient: patient)
+      create(:pipeline, :with_computations, project: project)
 
-      visit patients_path
+      visit projects_path
 
       expect(page).to have_selector(
         ".progress a:nth-child(1) #{bar_selector('segmentation')}"
@@ -135,7 +135,7 @@ RSpec.feature 'Patient browsing' do
     end
   end
 
-  context 'in the context of inspecting a given case' do
+  context 'in the context of inspecting a given project' do
     let(:details) do
       {
         status: :ok,
@@ -150,48 +150,48 @@ RSpec.feature 'Patient browsing' do
       }
     end
 
-    scenario 'lets the user to go back to the patients list' do
-      visit patient_path(patient)
+    scenario 'lets the user to go back to the projects list' do
+      visit project_path(project)
 
-      expect(page).to have_content I18n.t('patients.show.back')
+      expect(page).to have_content I18n.t('projects.show.back')
 
-      click_link I18n.t('patients.show.back')
+      click_link I18n.t('projects.show.back')
 
-      expect(current_path).to eq patients_path
+      expect(current_path).to eq projects_path
     end
 
     scenario 'shows pipelines list' do
-      pipeline = create(:pipeline, patient: patient, name: 'p1')
+      pipeline = create(:pipeline, project: project, name: 'p1')
 
-      visit patient_path(patient)
+      visit project_path(project)
 
       expect(page).to have_link('p1',
-                                href: patient_pipeline_path(patient, pipeline))
+                                href: project_pipeline_path(project, pipeline))
       expect(page).to have_content I18n.t("simple_form.options.pipeline.flow.#{pipeline.flow}")
       expect(page).to have_content pipeline.user.name
     end
 
     scenario 'don\'t show compare button when only one pipeline' do
-      visit patient_path(patient)
+      visit project_path(project)
 
       expect(page).
-        to_not have_selector "input[value='#{I18n.t('patients.pipelines.tab_compare.compare')}']"
+        to_not have_selector "input[value='#{I18n.t('projects.pipelines.tab_compare.compare')}']"
     end
 
     scenario 'show compare button when more than one pipeline' do
-      create(:pipeline, patient: patient)
-      create(:pipeline, patient: patient)
+      create(:pipeline, project: project)
+      create(:pipeline, project: project)
 
-      visit patient_path(patient)
+      visit project_path(project)
 
       expect(page).
-        to have_selector "input[value='#{I18n.t('patients.pipelines.tab_compare.compare')}']"
+        to have_selector "input[value='#{I18n.t('projects.pipelines.tab_compare.compare')}']"
     end
 
     scenario 'user can create new manual pipeline' do
       expect(Pipelines::StartRunnable).to_not receive(:new)
       expect do
-        visit patient_path(patient)
+        visit project_path(project)
         click_link 'Set up new pipeline'
         fill_in 'Name', with: 'my new manual pipeline'
         select 'manual', from: 'Mode'
@@ -200,7 +200,7 @@ RSpec.feature 'Patient browsing' do
 
       pipeline = Pipeline.last
 
-      expect(pipeline.patient).to eq patient
+      expect(pipeline.project).to eq project
       expect(pipeline.name).to eq 'my new manual pipeline'
       expect(pipeline).to be_manual
     end
@@ -210,7 +210,7 @@ RSpec.feature 'Patient browsing' do
         to receive(:new).and_return(double(call: true))
 
       expect do
-        visit patient_path(patient)
+        visit project_path(project)
         click_link 'Set up new pipeline'
         fill_in 'Name', with: 'my new automatic pipeline'
         select 'automatic', from: 'Mode'
@@ -219,13 +219,13 @@ RSpec.feature 'Patient browsing' do
 
       pipeline = Pipeline.last
 
-      expect(pipeline.patient).to eq patient
+      expect(pipeline.project).to eq project
       expect(pipeline.name).to eq 'my new automatic pipeline'
       expect(pipeline).to be_automatic
     end
 
     scenario 'error details are displayed when pipeline cannot be created' do
-      visit patient_path(patient)
+      visit project_path(project)
       click_link 'Set up new pipeline'
       click_on 'Set up new pipeline'
 
@@ -233,10 +233,10 @@ RSpec.feature 'Patient browsing' do
     end
   end
 
-  context 'in the context of inspecting a given case pipeline' do
+  context 'in the context of inspecting a given project pipeline' do
     scenario 'displays basic info about a pipeline' do
-      pipeline = create(:pipeline, patient: patient)
-      visit patient_pipeline_path(patient, pipeline)
+      pipeline = create(:pipeline, project: project)
+      visit project_pipeline_path(project, pipeline)
 
       expect(page).to have_content pipeline.name
       expect(page).to have_content I18n.t("simple_form.options.pipeline.flow.#{pipeline.flow}")
@@ -244,9 +244,9 @@ RSpec.feature 'Patient browsing' do
     end
 
     scenario 'lists pipeline steps in correct flow order' do
-      pipeline = create(:pipeline, :with_computations, patient: patient)
+      pipeline = create(:pipeline, :with_computations, project: project)
 
-      visit patient_pipeline_path(patient, pipeline)
+      visit project_pipeline_path(project, pipeline)
 
       expect(page).to have_selector(
         '#computations li:nth-child(1) a', text: I18n.t('steps.segmentation.title')
@@ -269,16 +269,16 @@ RSpec.feature 'Patient browsing' do
     end
 
     scenario 'shows alert when no computation defined' do
-      pipeline = create(:pipeline, patient: patient, name: 'p1')
-      visit patient_pipeline_path(patient, pipeline)
+      pipeline = create(:pipeline, project: project, name: 'p1')
+      visit project_pipeline_path(project, pipeline)
 
-      expect(page).to have_content I18n.t('patients.pipelines.show.no_computations')
+      expect(page).to have_content I18n.t('projects.pipelines.show.no_computations')
     end
 
     context 'with automatic computations' do
       let(:pipeline) do
         pipeline = build(:pipeline,
-                         patient: patient,
+                         project: project,
                          name: 'p1', user: user, mode: :automatic)
         Pipelines::Create.new(pipeline, {}).call
       end
@@ -291,7 +291,7 @@ RSpec.feature 'Patient browsing' do
         expect(Pipelines::StartRunnable).to receive_message_chain(:new, :call)
         expect_any_instance_of(Computation).to_not receive(:run)
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
         select('t1')
         click_button computation_run_text(computation)
 
@@ -304,7 +304,7 @@ RSpec.feature 'Patient browsing' do
     context 'with manual computations' do
       let(:pipeline) do
         pipeline = build(:pipeline,
-                         patient: patient,
+                         project: project,
                          flow: 'avr_from_scan_rom',
                          name: 'p1', user: user, mode: :manual)
         Pipelines::Create.new(pipeline, {}).call
@@ -315,14 +315,14 @@ RSpec.feature 'Patient browsing' do
 
       scenario 'redirects into first defined computation' do
         computation = pipeline.computations.first
-        visit patient_pipeline_path(patient, pipeline)
+        visit project_pipeline_path(project, pipeline)
 
         expect(current_path).
-          to eq patient_pipeline_computation_path(patient, pipeline, computation)
+          to eq project_pipeline_computation_path(project, pipeline, computation)
       end
 
       scenario 'all possible computations are displayed' do
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
 
         pipeline.steps.each do |step|
           title = I18n.t("steps.#{step.name}.title")
@@ -335,7 +335,7 @@ RSpec.feature 'Patient browsing' do
 
         expect(Rimrock::StartJob).to receive(:perform_later)
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
         select('bar')
         click_button computation_run_text(computation)
 
@@ -350,7 +350,7 @@ RSpec.feature 'Patient browsing' do
         computation.update_attributes(revision: 'my-revision',
                                       started_at: Time.zone.now)
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
 
         expect(page).to have_link 'my-revision'
         expect(page).
@@ -361,7 +361,7 @@ RSpec.feature 'Patient browsing' do
         computation = pipeline.computations.
                       find_by(pipeline_step: '0d_models')
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
 
         expect(page).
           to_not have_link href: 'https://gitlab.com/eurvalve/0dmodel/tree'
@@ -370,7 +370,7 @@ RSpec.feature 'Patient browsing' do
       scenario 'unable to start rimrock computation when version is not chosen' do
         mock_rimrock_computation_ready_to_run
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
         click_button computation_run_text(computation)
 
         expect(page).to have_content 'can\'t be blank'
@@ -380,11 +380,11 @@ RSpec.feature 'Patient browsing' do
         mock_webdav_computation_ready_to_run
         computation = pipeline.computations.
                       find_by(pipeline_step: 'segmentation')
-        create(:data_file, data_type: :image, patient: patient)
+        create(:data_file, data_type: :image, project: project)
 
         expect(Webdav::StartJob).to receive(:perform_later)
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
         select('Workflow 5 (Mitral Valve TEE Segmentation)')
         click_button computation_run_text(computation)
       end
@@ -393,9 +393,9 @@ RSpec.feature 'Patient browsing' do
         mock_webdav_computation_ready_to_run
         computation = pipeline.computations.
                       find_by(pipeline_step: 'segmentation')
-        create(:data_file, data_type: :image, patient: patient)
+        create(:data_file, data_type: :image, project: project)
 
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
         click_button computation_run_text(computation)
 
         expect(page).to have_content 'can\'t be blank'
@@ -415,20 +415,20 @@ RSpec.feature 'Patient browsing' do
       end
 
       scenario 'computation alert is displayed when no required input data' do
-        visit patient_pipeline_computation_path(patient, pipeline, computation)
+        visit project_pipeline_computation_path(project, pipeline, computation)
         msg_key = "steps.#{computation.pipeline_step}.cannot_start"
 
         expect(page).to have_content I18n.t(msg_key)
       end
 
-      context 'when computing for patient\'s wellbeing' do
+      context 'when computing for project\'s wellbeing' do
         scenario 'displays computation stdout and stderr' do
           allow_any_instance_of(Computation).to receive(:runnable?).and_return(true)
           computation.update_attributes(started_at: Time.current,
                                         stdout_path: 'http://download/stdout.pl',
                                         stderr_path: 'http://download/stderr.pl')
 
-          visit patient_pipeline_computation_path(patient, pipeline, computation)
+          visit project_pipeline_computation_path(project, pipeline, computation)
 
           expect(page).to have_link('stdout', href: 'http://files/stdout.pl')
           expect(page).to have_link('stderr', href: 'http://files/stderr.pl')
@@ -440,7 +440,7 @@ RSpec.feature 'Patient browsing' do
         #   allow_any_instance_of(Computation).to receive(:runnable?).and_return(true)
         #   computation.update_attributes(status: 'new', started_at: Time.current)
         #
-        #   visit patient_pipeline_computation_path(patient, pipeline, computation)
+        #   visit project_pipeline_computation_path(project, pipeline, computation)
         #
         #   expect(page).to have_content('New')
         #
@@ -454,7 +454,7 @@ RSpec.feature 'Patient browsing' do
         #   allow_any_instance_of(Computation).to receive(:runnable?).and_return(true)
         #   computation.update_attributes(status: 'new', started_at: Time.current)
         #
-        #   visit patient_pipeline_computation_path(patient, pipeline, computation)
+        #   visit project_pipeline_computation_path(project, pipeline, computation)
         #
         #   expect(page).to have_content('New')
         #
