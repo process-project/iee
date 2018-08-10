5d591c57c512cf3b3c2ec662f46f2f8052dde2ec
Tomasz Gubala <t.gubala@cyfronet.pl>
2018-08-10 16:45:24 +0200

    serializers specs

---
+++ spec/serializers/simple_pipeline_serializer_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rspec/json_expectations'
+
+RSpec.describe SimplePipelineSerializer do
+  it 'serializes basic pipeline data' do
+    pipeline = create(:pipeline, :with_computations, iid: 1, flow: 'unused_steps')
+
+    json = JSON.parse(described_class.new(pipeline).serialized_json)
+
+    expect(json).to include_json(
+      data: {
+        type: 'pipeline',
+        id: pipeline.iid.to_s,
+        attributes: {
+          name: pipeline.name,
+          flow: "unused_steps"
+        }
+      }
+    )
+  end
+end
