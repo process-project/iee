bbb8135f3651c4c81a0f05314e338f0d1cf541f6
JanKapala <jankapala@gmail.com>
2018-08-24 09:20:42 +0200

    Excised procedure_status from app/models/project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,28 +1,17 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  enum procedure_status: [
-    :not_started,
-    :imaging_uploaded,
-    :segmentation_ready,
-    :virtual_model_ready,
-    :after_blood_flow_simulation,
-    :after_parameter_estimation,
-    :after_heart_simulation
-  ]
 
   has_many :data_files, dependent: :destroy
   has_many :pipelines,
            -> { order(iid: :asc) },
            dependent: :destroy
 
-  validates :project_name, :procedure_status, presence: true
+  validates :project_name, presence: true
   validates :project_name, uniqueness: true
   validates :project_name, format: { with: /\A[a-zA-Z0-9_\-.]+\z/ }
 
   default_scope { order('project_name asc') }
-
-  after_touch :update_procedure_status
 
   def to_param
     project_name
@@ -65,42 +54,4 @@ class Project < ApplicationRecord
   # rubocop:disable CyclomaticComplexity
   # rubocop:disable Metrics/AbcSize
   # rubocop:disable Metrics/PerceivedComplexity
-  def update_procedure_status
-    data_files.reload
-    # This should go from the most advanced status to the least advanced one.
-    if heart_model_output_exist? then after_heart_simulation!
-    elsif estimated_parameters_exist? then after_parameter_estimation!
-    elsif blood_flow_result_and_model_exist? then after_blood_flow_simulation!
-    elsif fluid_and_ventricle_virtual_models_exist? then virtual_model_ready!
-    elsif segmentation_output_exist? then segmentation_ready!
-    elsif segmentation_input_exist? then imaging_uploaded!
-    else not_started!
-    end
-  end
-
-  def fluid_and_ventricle_virtual_models_exist?
-    data_files.any?(&:fluid_virtual_model?) &&
-      data_files.any?(&:ventricle_virtual_model?)
-  end
-
-  def blood_flow_result_and_model_exist?
-    data_files.reload.any?(&:blood_flow_result?) &&
-      data_files.reload.any?(&:blood_flow_model?)
-  end
-
-  def estimated_parameters_exist?
-    data_files.reload.any?(&:estimated_parameters?)
-  end
-
-  def heart_model_output_exist?
-    data_files.reload.any?(&:heart_model_output?)
-  end
-
-  def segmentation_input_exist?
-    data_files.reload.any?(&:image?)
-  end
-
-  def segmentation_output_exist?
-    data_files.reload.any?(&:segmentation_result?)
-  end
 end
