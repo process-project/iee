362ffe4b0390f8a54fe40eb86211f8d0e36b1ae5
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-10-09 10:14:45 +0200

    Update to factory_bot_rails 4.11.1
    Factory bot will be deprecating static attributes declaration (in 5.0.0).
    All statics was converted into new schema using block.

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -4,15 +4,15 @@ FactoryBot.define do
   factory :resource do
     sequence(:name) { |n| "resource_#{n}" }
     sequence(:path) { |n| "/resource/path/#{n}" }
-    resource_type :local
+    resource_type { :local }
     service
 
     trait :global do
-      resource_type :global
+      resource_type { :global }
     end
 
     trait :local do
-      resource_type :local
+      resource_type { :local }
     end
 
     factory :global_resource, traits: [:global]
