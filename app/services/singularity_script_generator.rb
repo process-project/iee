9a2e3adb0088b5daf3d030c7491999ec46208a0d
JanKapala <jankapala@gmail.com>
2018-12-13 13:50:02 +0100

    Fix basic singularity script generator

--- app/services/singularity_script_generator.rb
+++ app/services/singularity_script_generator.rb
@@ -12,24 +12,24 @@ class SingularityScriptGenerator
   end
 
   def call
-    "
-    #!/bin/bash -l
-    #SBATCH -N 1
-    #SBATCH --ntasks-per-node=1
-    #SBATCH --time=00:05:00
-    #SBATCH -A process
-    #SBATCH -p plgrid-testing
-    #SBATCH --output /net/archive/groups/plggprocess/Mock/slurm_outputs/slurm-%j.out
-    #SBATCH --error /net/archive/groups/plggprocess/Mock/slurm_outputs/slurm-%j.err
+    <<~CODE
+      #!/bin/bash -l
+      #SBATCH -N 1
+      #SBATCH --ntasks-per-node=1
+      #SBATCH --time=00:05:00
+      #SBATCH -A process
+      #SBATCH -p plgrid-testing
+      #SBATCH --output /net/archive/groups/plggprocess/Mock/slurm_outputs/slurm-%j.out
+      #SBATCH --error /net/archive/groups/plggprocess/Mock/slurm_outputs/slurm-%j.err
 
-    ## Running container using singularity
-    module load plgrid/tools/singularity/stable
+      ## Running container using singularity
+      module load plgrid/tools/singularity/stable
 
-    cd $SCRATCHDIR
+      cd $SCRATCHDIR
 
-    singularity pull --name container.simg #{@registry_url}#{@container_name} 
-    singularity run container.simg
-    "
+      singularity pull --name container.simg #{@registry_url}#{@container_name} 
+      singularity run container.simg
+    CODE
   end
 
 end
