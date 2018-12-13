91f250598639d710d3b9476598c4f99d0de1df9b
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2018-12-13 13:35:30 +0100

    Fix bug in module name

--- app/services/pipeline_steps/singularity/builder.rb
+++ app/services/pipeline_steps/singularity/builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PipelineSteps
-  module Rimrock
+  module Singularity
     class Builder
       def initialize(pipeline, name, params = {})
         @pipeline = pipeline
