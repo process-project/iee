9ff0fdc0eae703111d3679a6d0537401f6078059
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2018-12-18 13:14:12 +0100

    Add validations to Singularity Computation

--- app/models/singularity_computation.rb
+++ app/models/singularity_computation.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class SingularityComputation < Computation
-  validates :script, presence: true, unless: :created?
+  validates :script, presence: true, unless: :created?,
+  validates :container_name, presence: true,
+  validates :registry_url, presence: true
 end
