dec31a0550e01d40c2c024e25a87146121964613
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2018-12-18 15:11:28 +0100

    Fixed wrong Rimrock computation validations

--- app/models/rimrock_computation.rb
+++ app/models/rimrock_computation.rb
@@ -3,5 +3,5 @@
 class RimrockComputation < Computation
   validates :script, presence: true, unless: :created?
   validates :input_path, :output_path, :working_file_name, :run_mode, absence: true
-  validates :tag_or_branch, unless: :created?
+  validates :tag_or_branch, presence: true, unless: :created?
 end
