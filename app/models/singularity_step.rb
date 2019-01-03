aeeeb39586414f4d0ff88314575b5e72e535a330
JanKapala <jankapala@gmail.com>
2019-01-03 19:23:23 +0100

    Add a container_tag to the constructor

--- app/models/singularity_step.rb
+++ app/models/singularity_step.rb
@@ -3,17 +3,18 @@
 class SingularityStep < Step
   attr_reader :repository, :file
 
-  def initialize(name, registry_url, container_name)
+  def initialize(name, registry_url, container_name, container_tag)
     super(name, [])
     @registry_url = registry_url
     @container_name = container_name
+    @container_tag = container_tag
   end
 
   def builder_for(pipeline, params)
-    PipelineSteps::Singularity::Builder.new(pipeline, name, params)
+    PipelineSteps::Singularity::Builder.new(pipeline, name, @registry_url, @container_name, @container_tag)
   end
 
   def runner_for(computation, options = {})
-    PipelineSteps::Singularity::Runner.new(computation, @registry_url, @container_name, options)
+    PipelineSteps::Singularity::Runner.new(computation, @registry_url, @container_name, @container_tag, options)
   end
 end
