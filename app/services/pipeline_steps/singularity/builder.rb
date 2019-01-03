232faaafbced855759c2c6f55ea07b4f38c76de1
JanKapala <jankapala@gmail.com>
2019-01-03 19:24:23 +0100

    Add a container_tag to the Singularity Builder

--- app/services/pipeline_steps/singularity/builder.rb
+++ app/services/pipeline_steps/singularity/builder.rb
@@ -3,21 +3,22 @@
 module PipelineSteps
   module Singularity
     class Builder
-      def initialize(pipeline, name, params = {})
+      def initialize(pipeline, name, registry_url, container_name, container_tag)
         @pipeline = pipeline
         @name = name
-        @registry_url = params[:registry_url]
-        @container_name = params[:container_name]
+        @registry_url = registry_url
+        @container_name = container_name
+        @container_tag = container_tag
       end
 
-      # TODO
       def call
         SingularityComputation.create!(
           pipeline: @pipeline,
           user: @pipeline.user,
           pipeline_step: @name,
-          registry_url: 'Placeholder registry url',
-          container_name: 'Placeholder container name'
+          registry_url: @registry_url,
+          container_name: @container_name,
+          container_tag: @container_tag
         )
       end
     end
