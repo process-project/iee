571dc3d61d7a6d9874426357ecf2c4e82fe595b0
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-01-16 20:59:51 +0100

    Add dependent to Container Registry model

--- app/models/container_registry.rb
+++ app/models/container_registry.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ContainerRegistry < ApplicationRecord
-  has_many :computations
+  has_many :computations, dependent: :destroy
   validates :registry_url, uniqueness: true, presence: false
 end
