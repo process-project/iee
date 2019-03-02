eba2a90356ed28cc76b11a93b08ffcd50b64ebff
JanKapala <jankapala@gmail.com>
2019-03-02 23:20:54 +0100

    Rails update from 5.1 to 5.2

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,6 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
 require 'rails/all'
-
-require File.expand_path('lib/jwt/config')
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,43 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Vapor
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.,
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    config.middleware.insert_before Warden::Manager, Rack::Attack
-
-    # Custom error pages
-    config.exceptions_app = routes
-
-    config.constants = config_for(:application)
-
-    config.jwt = Jwt::Config.new(config.constants['jwt'])
-    config.clock = Struct.new(:update).
-                   new((config.constants['clock']['update'] || 30).seconds)
-
-    # Overrides eurvalve locales if override dir exists
-    Dir.chdir('./config/locales') do
-      Dir.foreach('./') do |item|
-        if File.directory?(item) && !['.', '..'].include?(item)
-          config.i18n.load_path += Dir[root.join('config', 'locales', item, '*.yml')]
-        end
-      end
-    end
-
-    redis_url_string = config.constants['redis_url']
-
-    # Redis::Store does not handle Unix sockets well, so let's do it for them
-    redis_config_hash = Redis::Store::Factory.
-                        extract_host_options_from_uri(redis_url_string)
-    redis_uri = URI.parse(redis_url_string)
-    redis_config_hash[:path] = redis_uri.path if redis_uri.scheme == 'unix'
-
-    redis_config_hash[:namespace] = 'cache:vapor'
-    redis_config_hash[:expires_in] = 90.minutes # Cache should not grow forever
-    config.cache_store = :redis_store, redis_config_hash
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# rubocop:disable BlockLength
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -17,10 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -33,8 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # `config.assets.precompile` and `config.assets.version`
-  # have moved to config/initializers/assets.rb
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -42,6 +37,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -53,10 +51,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
+  config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -64,26 +62,12 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "vapor_#{Rails.env}"
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {
-    host: ENV['HOST'] || 'valve.cyfronet.pl'
-  }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: ENV['MAIL_HOST'],
-    port: 25,
-    user_name: ENV['MAIL_USER'],
-    password: ENV['MAIL_PASS'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -99,7 +83,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
@@ -107,9 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_cable.allowed_request_origins = [
-    %r{https://valve.*\.cyfronet\.pl}
-  ]
 end
-# rubocop:enable BlockLength
