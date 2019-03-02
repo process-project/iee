7044dc611ec0c7a0c958c71c37e120e73866dcb2
JanKapala <jankapala@gmail.com>
2019-03-02 23:32:32 +0100

    Update syntax in computations.rb due to new rules

--- spec/factories/computations.rb
+++ spec/factories/computations.rb
@@ -9,27 +9,27 @@ FactoryBot.define do
     pipeline
 
     factory :webdav_computation, class: 'WebdavComputation' do
-      pipeline_step 'placeholder_step'
-      input_path '/inputs'
-      output_path '/outputs'
-      run_mode 'Workflow 3 (TEE Aortic Valve Segmentation)'
+      pipeline_step { 'segmentation' }
+      input_path { '/inputs' }
+      output_path { '/outputs' }
+      run_mode { 'Workflow 3 (TEE Aortic Valve Segmentation)' }
     end
 
     factory :rimrock_computation, class: 'RimrockComputation' do
-      pipeline_step 'placeholder_step'
-      input_path nil
-      output_path nil
-      script 'SCRIPT'
-      tag_or_branch 'master'
-      revision '1234'
+      pipeline_step { '0d_models' }
+      input_path { nil }
+      output_path { nil }
+      script { 'SCRIPT' }
+      tag_or_branch { 'master' }
+      revision { '1234' }
     end
 
     factory :singularity_computation, class: 'SingularityComputation' do
-      pipeline_step 'singularity_placeholder_step'
-      script 'SCRIPT'
-      container_name 'lolcow'
-      container_registry nil
-      container_tag 'latest'
+      pipeline_step { 'singularity_placeholder_step' }
+      script { 'SCRIPT' }
+      container_name { 'lolcow' }
+      container_registry { nil }
+      container_tag { 'latest' }
     end
   end
 end
