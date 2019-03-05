3d70271f03586baea9673a5c8757b13f8d0a371e
Tomasz Gubala <t.gubala@cyfronet.pl>
2019-03-05 14:45:40 +0100

    WIP: refactoring configured?

--- app/models/rimrock_computation.rb
+++ app/models/rimrock_computation.rb
@@ -3,5 +3,9 @@
 class RimrockComputation < Computation
   validates :script, presence: true, unless: :created?
   validates :input_path, :output_path, :working_file_name, :run_mode, absence: true
-  validates :tag_or_branch, presence: true, unless: -> { created? && automatic? }
+  validates :tag_or_branch, presence: true, unless: :created?
+
+  def configured?
+    super && tag_or_branch.present? && script.present?
+  end
 end
