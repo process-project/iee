9e381436aa8681f9e50290160386c315d009c1b8
JanKapala <jankapala@gmail.com>
2019-03-24 15:53:04 +0100

    Add StagingIn Update Job implementation

---
+++ app/jobs/staging_in/update_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module StagingIn
+  class UpdateJob < ApplicationJob
+    queue_as :computation
+
+    def perform(computation)
+      StagingIn::Update.new(computation,
+                         	on_finish_callback: PipelineUpdater,
+                         	updater: ComputationUpdater).call
+    end
+  end
+end
