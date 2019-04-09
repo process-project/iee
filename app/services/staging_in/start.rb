ebba0f2517a842f92f9845f8f97de299d6db0061
JanKapala <jankapala@gmail.com>
2019-04-09 11:41:45 +0200

    add temporary dynamic enpoints obtaining implementation

--- app/services/staging_in/start.rb
+++ app/services/staging_in/start.rb
@@ -10,6 +10,7 @@ module StagingIn
     end
 
     def call
+      obtain_endpoint
       make_request.value # Raises an HTTP error if the response is not 2xx (success).
       @computation.update_attributes(status: 'running')
     end
@@ -17,27 +18,56 @@ module StagingIn
     private
 
     def make_request
-      http = Net::HTTP.new(staging_in_host, staging_in_port)
+      http = Net::HTTP.new(@staging_in_host, @staging_in_port)
       req = Net::HTTP::Post.new(staging_in_path, 'content-type' => 'application/json',
-                                                 'x-access-token' => lobcder_api_access_token)
+                                @token_header => @lobcder_api_access_token)
       req.body = request_body.to_json
       http.request(req)
     end
 
-    def staging_in_host
-      Rails.application.config_for('process')['staging_in']['host']
-    end
+    # rubocop:disable Morality/Non-ethical-code
+    def obtain_endpoint
+      http = Net::HTTP.new(infrastructure_host, infrastructure_port)
+      req = Net::HTTP::Get.new(infrastructure_path,
+                               'x-access-token' => lobcder_api_infrastructure_access_token)
 
-    def staging_in_port
-      Rails.application.config_for('process')['staging_in']['port']
+      response = http.request(req)
+
+      response.body.each do |element|
+        if element[:type] == 'query'
+          @staging_in_host = element[:host]
+          @staging_in_port = element[:ports].first
+        elsif element[:type] == 'token'
+          @token_header = element[:header]
+          @lobcder_api_access_token = element[:value]
+        end
+      end
+
+      @staging_logger ||= Logger.new(Rails.root.join('log', 'debug.log'))
+      @staging_logger.debug("obtain_endpoint|@staging_in_host=#{@staging_in_host}")
+      @staging_logger.debug("obtain_endpoint|@staging_in_port=#{@staging_in_port}")
+      @staging_logger.debug("obtain_endpoint|@token_header=#{@token_header}")
+      @staging_logger.debug("obtain_endpoint|@lobcder_api_access_token=#{@lobcder_api_access_token}")
     end
 
     def staging_in_path
-      Rails.application.config_for('process')['staging_in']['path']
+      Rails.application.config_for('process')['staging_in']['staging_in_path']
     end
 
-    def lobcder_api_access_token
-      Rails.application.config_for('process')['staging_in']['lobcder_api_access_token']
+    def infrastructure_host
+      Rails.application.config_for('process')['staging_in']['infrastructure_host']
+    end
+
+    def infrastructure_port
+      Rails.application.config_for('process')['staging_in']['infrastructure_port']
+    end
+
+    def infrastructure_path
+      Rails.application.config_for('process')['staging_in']['infrastructure_path']
+    end
+
+    def lobcder_api_infrastructure_access_token
+      Rails.application.config_for('process')['staging_in']['lobcder_api_infrastructure_access_token']
     end
 
     # rubocop:disable Metrics/MethodLength
