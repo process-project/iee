4e0dbbe104e7c7fb7d0a3dab4eb05295819aa2bc
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-04-10 12:08:06 +0200

    Printing gitlab endpoint

--- config/initializers/gitlab.rb
+++ config/initializers/gitlab.rb
@@ -3,4 +3,6 @@
 Gitlab.configure do |config|
   config.endpoint       = "https://#{ENV['GITLAB_HOST'] || 'gitlab.com'}/api/v4"
   config.private_token  = ENV['GITLAB_API_PRIVATE_TOKEN']
+
+  puts ">>>>>>>>>>>>>>>> gitlab config endpoint: #{config.endpoint}"
 end
