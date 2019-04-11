1b9087edcbed69e222d79c7e9d435c764cb350d4
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-04-11 15:54:17 +0200

    Add GetDynamicEndpointSpec

---
+++ spec/services/staging_in/get_dynamic_endpoint_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StagingIn::GetDynamicEndpoint do
+
+  it 'enpoint has required entries' do
+    endpoint = described_class.new.call
+
+    expect(endpoint['staging_in_host']).to_not be_nil
+    expect(endpoint['staging_in_port']).to match(/^[0-9]*$/)
+    expect(endpoint['token_header']).to_not be_nil
+    expect(endpoint['lobcder_api_access_token']).to_not be_nil
+  end
+
+end
