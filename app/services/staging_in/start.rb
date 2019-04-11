7db79c037369b34fdc1bc332ce6f7c05535b863d
JanKapala <jankapala@gmail.com>
2019-04-11 13:28:07 +0200

    Add dynamic endpoint handling

--- app/services/staging_in/start.rb
+++ app/services/staging_in/start.rb
@@ -10,6 +10,7 @@ module StagingIn
     end
 
     def call
+      @staging_logger.debug("Start -> call | before obtain_endpoint")
       obtain_endpoint
       make_request.value # Raises an HTTP error if the response is not 2xx (success).
       @computation.update_attributes(status: 'running')
@@ -17,37 +18,21 @@ module StagingIn
 
     private
 
+    def obtain_endpoint
+      endpoint = StagingIn::DynamicEndpoint.new.get_query_endpoint
+
+      @staging_in_host = endpoint['staging_in_host']
+      @staging_in_port = endpoint['staging_in_port']
+      @token_header = endpoint['token_header']
+      @lobcder_api_access_token = endpoint['lobcder_api_access_token']
+    end
+
     def make_request
       http = Net::HTTP.new(@staging_in_host, @staging_in_port)
       req = Net::HTTP::Post.new(staging_in_path, 'content-type' => 'application/json',
                                 @token_header => @lobcder_api_access_token)
       req.body = request_body.to_json
       http.request(req)
-    end
-
-    # rubocop:disable Morality/Non-ethical-code
-    def obtain_endpoint
-      http = Net::HTTP.new(infrastructure_host, infrastructure_port)
-      req = Net::HTTP::Get.new(infrastructure_path,
-                               'x-access-token' => lobcder_api_infrastructure_access_token)
-
-      response = http.request(req)
-
-      response.body.each do |element|
-        if element[:type] == 'query'
-          @staging_in_host = element[:host]
-          @staging_in_port = element[:ports].first
-        elsif element[:type] == 'token'
-          @token_header = element[:header]
-          @lobcder_api_access_token = element[:value]
-        end
-      end
-
-      @staging_logger ||= Logger.new(Rails.root.join('log', 'debug.log'))
-      @staging_logger.debug("obtain_endpoint|@staging_in_host=#{@staging_in_host}")
-      @staging_logger.debug("obtain_endpoint|@staging_in_port=#{@staging_in_port}")
-      @staging_logger.debug("obtain_endpoint|@token_header=#{@token_header}")
-      @staging_logger.debug("obtain_endpoint|@lobcder_api_access_token=#{@lobcder_api_access_token}")
     end
 
     def staging_in_path
