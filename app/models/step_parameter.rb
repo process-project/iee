d22d6351411e9e321811d6cbf9c4cd024aab2c76
JanKapala <jankapala@gmail.com>
2019-05-10 14:34:24 +0200

    Adjust step parameter class for new step parameters approach

--- app/models/step_parameter.rb
+++ app/models/step_parameter.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 class StepParameter < ApplicationRecord
+	attr_reader :name, :label, :description, :rank, :datatype, :default
   has_and_belongs_to_many :singularity_script_blueprints
+
+  def initialize(label, name, description, rank, datatype, default, values = [])
+  	                      @label =  label,
+                          @name = name,
+                          @description = description,
+                          @rank = rank,
+                          @datatype = datatype,
+                          @default = default,
+                          @values =  values
+  end
+
+  def values
+  	eval(read_attribute(:values))
+  end
 end
