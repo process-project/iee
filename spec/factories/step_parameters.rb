12c7b26018169dc9df8d7ae28e94f6aa266b997f
JanKapala <jankapala@gmail.com>
2019-05-10 14:40:51 +0200

    Add a stub spec implementation for step parameters

---
+++ spec/factories/step_parameters.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :step_parameter do
+    label "MyString"
+    name "MyString"
+    description "MyString"
+    rank 1
+    datatype "MyString"
+    default "MyString"
+    values "MyString"
+  end
+end
