b7e2f4a08dd1ddc56ce2eadce8d6fa65789837bc
JanKapala <jankapala@gmail.com>
2019-06-01 00:06:36 +0200

    Update singularity runner spec

--- spec/services/pipeline_steps/singularity/runner_spec.rb
+++ spec/services/pipeline_steps/singularity/runner_spec.rb
@@ -10,42 +10,27 @@ RSpec.describe PipelineSteps::Singularity::Runner do
     create(:pipeline, flow: 'singularity_placeholder_pipeline')
   end
 
+  # let(:computation) do
+  #   create(:computation,
+  #          pipeline_step: 'placeholder_step')
+  # end
+
   let(:computation) do
-    create(:computation,
-           pipeline_step: 'placeholder_step')
-  end
-
-  let(:singularity_computation) do
     create(:singularity_computation,
-           pipeline: pipeline,
-           pipeline_step: 'singularity_placeholder_step')
+           pipeline: singularity_pipeline,
+           pipeline_step: 'singularity_placeholder_step',
+           container_name: 'test_name',
+           container_tag: 'test_tag',
+           hpc: 'test_hpc',
+           parameter_values: {label1: 'w1', label2: 'w2', label3: 'w3'})
   end
 
-  let(:container_name) { 'vsoch/hello-world' }
-  let(:container_tag) { 'latest' }
-  script = <<~CODE
-    #!/bin/bash -l
-    #SBATCH -N 1
-    #SBATCH --ntasks-per-node=1
-    #SBATCH --time=00:05:00
-    #SBATCH -A process1
-    #SBATCH -p plgrid-testing
-    #SBATCH --output /net/archive/groups/plggprocess/Mock/slurm_outputs/slurm-%%j.out
-    #SBATCH --error /net/archive/groups/plggprocess/Mock/slurm_outputs/slurm-%%j.err
-
-    ## Running container using singularity
-    module load plgrid/tools/singularity/stable
-
-    cd $SCRATCHDIR
-
-    singularity pull --name container.simg %<registry_url>s%<container_name>s:%<tag>s
-    singularity run container.simg
-  CODE
-
-  SingularityScriptBlueprint.create!(container_name: 'vsoch/hello-world',
-                                     container_tag: 'latest',
-                                     hpc: 'Prometheus',
-                                     script_blueprint: script)
+  let!(:singularity_script_blueprint) do
+    create(:singularity_script_blueprint,
+           container_name: computation.container_name,
+           container_tag: computation.container_tag,
+           hpc: computation.hpc)
+  end
 
   subject do
     described_class.new(computation,
@@ -53,21 +38,17 @@ RSpec.describe PipelineSteps::Singularity::Runner do
   end
 
   context 'container step running' do
-    # it_behaves_like 'runnable step'
+    it_behaves_like 'runnable step'
 
     it 'starts a Rimrock job' do
       expect(Rimrock::StartJob).to receive(:perform_later)
-
       subject.call
     end
 
-    it 'creates computation with script returned by singularity script generator' do
-      # subject.call
+    it 'creates singularity computation script' do
+      subject.call
 
-      # expect(computation.script).to include container_registry.registry_url +
-      #                                       container_name +
-      #                                       ':' +
-      #                                       container_tag
+      expect(computation.script).to_not be_empty
     end
 
     it 'set job_id to null while restarting computation' do
