99be00f7651daf2901de64fbc6139124605f0b92
JanKapala <jankapala@gmail.com>
2019-06-05 22:48:12 +0200

    Basic implementation of singularity step spec

--- spec/models/singularity_step_spec.rb
+++ spec/models/singularity_step_spec.rb
@@ -4,8 +4,33 @@ require 'rails_helper'
 require 'models/step_shared_examples'
 
 RSpec.describe SingularityStep do
-  subject { SingularityStep.new('TestStep') }
-  let(:pipeline) { create(:pipeline) }
+  @my_logger ||= Logger.new("#{Rails.root}/log/debug.log")
+  # before :all do
+  # end
+  step_name = "test_singularity_step"
+  container_name = "test_container_name"
+  container_tag = "test_tag"
 
-  it_behaves_like 'pipeline step builder'
+  SingularityRegistry.add_step(step_name, [container_name])
+  SingularityRegistry.add_container(container_name, [container_tag])
+
+  let!(:singularity_script_blueprint) { create(:singularity_script_blueprint,
+                                              container_name: container_name,
+                                              container_tag: container_tag) }
+
+  @my_logger.info("SingularityScriptBlueprint.all: #{SingularityScriptBlueprint.where(container_name: container_name)}")
+
+  subject { SingularityStep.new(step_name) }
+
+  context "Step initialized with specific name" do
+    xit "obtains specific parameters from apropriate blueprint" do
+        expect(subject.parameters).to include(singularity_script_blueprint.parameters)
+    end 
+  end
+
+  SingularityRegistry.remove_step(step_name)
+  SingularityRegistry.remove_container(container_name)
+
+  # after :all do
+  # end
 end
