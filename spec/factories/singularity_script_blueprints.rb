c194fb4860c6813aefdfe2ba6983ad604caed389
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-06-18 15:43:38 +0200

    Fix factory bug

--- spec/factories/singularity_script_blueprints.rb
+++ spec/factories/singularity_script_blueprints.rb
@@ -7,32 +7,34 @@ FactoryBot.define do
     hpc 'Prometheus'
     script_blueprint 'parameter1 %<label1>s parameter2 %<label2>s parameter3 %<label3>s'
 
-    step_parameters [
-      StepParameter.new(
-        label: 'label1',
-        name: 'name',
-        description: 'desc',
-        rank: 0,
-        datatype: 'string',
-        default: 'default_value'
-      ),
-      StepParameter.new(
-        label: 'label2',
-        name: 'name',
-        description: 'desc',
-        rank: 0,
-        datatype: 'integer',
-        default: 1
-      ),
-      StepParameter.new(
-        label: 'label3',
-        name: 'name',
-        description: 'desc',
-        rank: 0,
-        datatype: 'multi',
-        default: 'first_value',
-        values: %w[first_value second_value third_value]
-      )
-    ]
+    step_parameters do
+      [
+        StepParameter.new(
+          label: 'label1',
+          name: 'name',
+          description: 'desc',
+          rank: 0,
+          datatype: 'string',
+          default: 'default_value'
+        ),
+        StepParameter.new(
+          label: 'label2',
+          name: 'name',
+          description: 'desc',
+          rank: 0,
+          datatype: 'integer',
+          default: 1
+        ),
+        StepParameter.new(
+          label: 'label3',
+          name: 'name',
+          description: 'desc',
+          rank: 0,
+          datatype: 'multi',
+          default: 'first_value',
+          values: %w[first_value second_value third_value]
+        )
+      ]
+    end
   end
 end
