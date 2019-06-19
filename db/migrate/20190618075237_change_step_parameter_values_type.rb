70e53cbf59a914f82e72e4cd321ea6f7f8826e44
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-06-19 13:05:33 +0200

    Delete obsolete comments

--- db/migrate/20190618075237_change_step_parameter_values_type.rb
+++ db/migrate/20190618075237_change_step_parameter_values_type.rb
@@ -5,16 +5,5 @@ class ChangeStepParameterValuesType < ActiveRecord::Migration[5.1]
 
   def down
     change_column :step_parameters, :values, :string
-    # result = execute "SELECT id, values FROM step_parameters WHERE values IS NOT null" 
-
-
-
-    # result.each do |row|
-    #   id = row['id']
-    #   converted_value = "'" + eval(row['parameter_values']).stringify_keys.to_json + "'"
-    #   execute "UPDATE computations
-    #                   SET parameter_values = #{converted_value}
-    #                   WHERE id = #{id}"
-    # end
   end 
 end
