98022161e8d1f2fd8776e39a0866107fc6037886
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-10-06 20:49:50 +0200

    Add views for ComputeSiteProxy

--- app/controllers/profiles/compute_site_proxy_controller.rb
+++ app/controllers/profiles/compute_site_proxy_controller.rb
@@ -1,2 +1,34 @@
 class Profiles::ComputeSiteProxyController < ApplicationController
+    def index
+      @compute_site_proxies = ComputeSiteProxy.all
+    end
+
+    def new
+      @compute_sites = ComputeSite.all
+      @compute_site_proxy = ComputeSiteProxy.new
+    end
+
+    def create
+      ComputeSiteProxy.create(user: current_user, 
+                        compute_site: ComputeSite.find(permitted_attributes[:compute_site]),
+                        value: permitted_attributes[:value])
+
+      redirect_to profile_compute_site_proxy_index_path
+    end
+
+    def edit
+      @compute_site_proxy = ComputeSiteProxy.find(params[:id])
+    end
+
+    def update
+      ComputeSiteProxy.find(params[:id]).update_attributes(value: permitted_attributes[:value])
+
+      redirect_to profile_compute_site_proxy_index_path
+    end
+
+    private
+
+    def permitted_attributes
+      params.require(:compute_site_proxy).permit(:value, :compute_site)
+    end
 end
--- app/models/compute_site_proxy.rb
+++ app/models/compute_site_proxy.rb
@@ -2,4 +2,8 @@ class ComputeSiteProxy < ApplicationRecord
 	belongs_to :user
 	belongs_to :compute_site
   validates :compute_site_id, uniqueness: {scope: :user_id}
+
+  def compute_site_name
+    compute_site.name
+  end
 end
