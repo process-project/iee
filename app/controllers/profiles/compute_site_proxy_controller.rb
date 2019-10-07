00c9d97517fb96c654deb99647194e720b82846a
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-10-07 12:02:36 +0200

    Add error rendering to controller

--- app/controllers/profiles/compute_site_proxy_controller.rb
+++ app/controllers/profiles/compute_site_proxy_controller.rb
@@ -9,11 +9,13 @@ class Profiles::ComputeSiteProxyController < ApplicationController
     end
 
     def create
-      ComputeSiteProxy.create(user: current_user, 
-                        compute_site: ComputeSite.find(permitted_attributes[:compute_site]),
-                        value: permitted_attributes[:value])
-
-      redirect_to profile_compute_site_proxy_index_path
+      if !create_proxy
+        flash[:alert] = "Unable to create compute site proxy"
+        redirect_to new_profile_compute_site_proxy_path
+      else
+        flash[:notice] = 'Compute site proxy added successfully'
+        redirect_to profile_compute_site_proxy_index_path
+      end
     end
 
     def edit
@@ -21,12 +23,26 @@ class Profiles::ComputeSiteProxyController < ApplicationController
     end
 
     def update
-      ComputeSiteProxy.find(params[:id]).update_attributes(value: permitted_attributes[:value])
-
-      redirect_to profile_compute_site_proxy_index_path
+      if !update_proxy
+        flash[:alert] = "Unable to update compute site proxy"
+        redirect_to edit_profile_compute_site_proxy_path
+      else
+        flash[:notice] = 'Compute site proxy updated successfully'
+        redirect_to profile_compute_site_proxy_index_path
+      end
     end
 
     private
+
+    def create_proxy
+      ComputeSiteProxy.create user: current_user, 
+                              compute_site: ComputeSite.find(permitted_attributes[:compute_site]), 
+                              value: permitted_attributes[:value]
+    end
+
+    def update_proxy
+      ComputeSiteProxy.find(params[:id]).update_attributes(value: permitted_attributes[:value])
+    end
 
     def permitted_attributes
       params.require(:compute_site_proxy).permit(:value, :compute_site)
