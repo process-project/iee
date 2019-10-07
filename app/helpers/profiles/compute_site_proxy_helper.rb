49df7a95043d127e4bcac023dd6d0fd59e6595bf
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-10-07 23:40:04 +0200

    Fix rubocop

--- app/helpers/profiles/compute_site_proxy_helper.rb
+++ app/helpers/profiles/compute_site_proxy_helper.rb
@@ -1,8 +1,12 @@
-module Profiles::ComputeSiteProxyHelper
-  def unset_compute_sites
-    all_compute_sites = ComputeSite.all.to_set
-    set_compute_sites = ComputeSiteProxy.where(user: current_user).map{|csp| csp.compute_site}.to_set
+# frozen_string_literal: true
 
-    all_compute_sites ^ set_compute_sites
+module Profiles
+  class ComputeSiteProxyHelper
+    def unset_compute_sites
+      all_compute_sites = ComputeSite.all.to_set
+      set_compute_sites = ComputeSiteProxy.where(user: current_user).map :compute_site.to_set
+
+      all_compute_sites ^ set_compute_sites
+    end
   end
 end
