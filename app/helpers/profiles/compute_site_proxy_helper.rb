ca69adfc0cb9fd60af727d933c164d69377c5c4e
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-10-07 11:59:55 +0200

    Added option to add new proxy only to an unset compute site

--- app/helpers/profiles/compute_site_proxy_helper.rb
+++ app/helpers/profiles/compute_site_proxy_helper.rb
@@ -1,2 +1,8 @@
 module Profiles::ComputeSiteProxyHelper
+  def unset_compute_sites
+    all_compute_sites = ComputeSite.all.to_set
+    set_compute_sites = ComputeSiteProxy.where(user: current_user).map{|csp| csp.compute_site}.to_set
+
+    all_compute_sites ^ set_compute_sites
+  end
 end
