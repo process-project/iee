2852af165fdcbbd2274863f3223fd88b2edae503
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-10-07 21:54:51 +0200

    Add nice looking view

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ end
 
 # Add compute sites to DB, besides Prometheus...
 ComputeSite.create(name: "LRZ")
-ComputeSite.create(name: "Amsterdam HPC")
+ComputeSite.create(name: "Amsterdam")
 
 # Test container for the Prometheus HPC
 script = <<~CODE
