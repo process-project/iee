cfec246f1a42c48b7d501cc1e6459a5dd605d87b
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-10-07 23:46:35 +0200

    Fix test

--- app/helpers/profiles/compute_site_proxy_helper.rb
+++ app/helpers/profiles/compute_site_proxy_helper.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-module Profiles
-  class ComputeSiteProxyHelper
-    def unset_compute_sites
-      all_compute_sites = ComputeSite.all.to_set
-      set_compute_sites = ComputeSiteProxy.where(user: current_user).map :compute_site.to_set
+module Profiles::ComputeSiteProxyHelper
+  def unset_compute_sites
+    all_compute_sites = ComputeSite.all.to_set
+    set_compute_sites = ComputeSiteProxy.where(user: current_user).map {|csp| csp.compute_site}.to_set
 
-      all_compute_sites ^ set_compute_sites
-    end
+    all_compute_sites ^ set_compute_sites
   end
 end
