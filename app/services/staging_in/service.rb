2b0070410a4a50a3e14b7e136f3d26e68ea2b91f
jan <jankapala@gmail.com>
2020-04-17 15:20:04 +0200

    Implement part of lobcder api methods

--- app/services/staging_in/service.rb
+++ app/services/staging_in/service.rb
@@ -1,6 +1,6 @@
-require 'faraday'
-
 module Staging
+  require 'faraday'
+  require 'securerandom'
   class Service
     def initialize(uc)
       @connection = meta_connection(uc)
@@ -19,25 +19,31 @@ module Staging
 
 
 
-
-
-
-    def copy(host_alias, [(dst, src), (dst, src)])
-      return track_id
+    def copy(commands)
+      copy_move_utility(commands, attribute_fetcher('copy_path'))
     end
 
-    def move(uc, host_alias, [(dst, src), (dst, src)])
-      return track_id
+    def move(commands)
+      copy_move_utility(commands, attribute_fetcher('move_path'))
     end
 
-    def status(uc, track_id)
-      return status
+    def status(track_id)
+      response = @connection.get "#{attribute_fetcher('status_path')}/#{track_id}"
+      JSON.parse(response.body)['status'] # We don't care about each one file status
     end
 
-    def list(uc, host_alias, path)
-      return ls_output
-    end
+    def list(host_alias, path)
+      payload = {
+          'name': host_alias,
+          'path': path
+      }.to_json
 
+      response = @connection.post do |req|
+        req.url attribute_fetcher('list_path'),
+        req.body = payload
+      end
+      JSON.parse(response.body)
+    end
 
     private
 
@@ -51,6 +57,50 @@ module Staging
 
     def meta_connection(uc, )
       connection
+    end
+
+    # utilities
+    def copy_move_utility(commands, api_path)
+      # commands argument example
+      # [
+      #     {
+      #         'dst': {
+      #             'name': 'lrzcluster',
+      #             'file': '/'
+      #         },
+      #         'src': {
+      #             'name': 'krk',
+      #             'file': '10M.dat'
+      #         }
+      #     }
+      # ]
+      payload = {
+          'id': SecureRandom.hex, # what should we send?
+          'webhook': webhook_info,
+          'cmd': commands
+      }.to_json
+
+      response = @connection.post do |req|
+        req.url attribute_fetcher(api_path)
+        req.body = payload
+      end
+      JSON.parse(response.body)['trackId']
+    end
+
+    def attribute_fetcher(attribute)
+      Rails.application.config_for('process')['staging'][attribute]
+    end
+
+    def webhook_info
+      { method: 'POST',
+        url: webhook_url,
+        headers: { 'x-staging-token': attribute_fetcher('staging_secret'),
+                   'content-type': 'application/json' } }
+    end
+
+    def webhook_url
+      Rails.application.routes.url_helpers.api_staging_url(protocol: 'https',
+                                                           host: ENV['HOST'])
     end
   end
 end
