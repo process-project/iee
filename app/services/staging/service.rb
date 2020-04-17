d4b6c0e35c808e6cbcd0067c417d4ce3f1505228
jan <jankapala@gmail.com>
2020-04-17 16:05:51 +0200

    Futher implementation of lobcder api service

--- app/services/staging/service.rb
+++ app/services/staging/service.rb
@@ -9,53 +9,71 @@ module Staging
 
     # TODO: Not implemented in LOBCDER, assuming that you can mkdir multiple paths
     def mkdir(host_alias, paths)
-      mkdir_path = 'TODO' # Doesnt exist yet - not implemented in LOBCDER
-
-      request_body = {
+      payload = {
           name: host_alias,
           path: paths
       }
 
-      connection.post do |req| # Only a guess on how could such a request look
-        req.url mkdir_path
+      @connection.post do |req| # Only a guess on how could such a request look
+        req.url attribute_fetcher('mkdir_path') # Doesnt exist yet - not implemented in LOBCDER
         req.headers['Content-Type'] = 'application/json'
-        req.body = request_body
+        req.body = payload
       end
     end
 
     # TODO: Not implemented in LOBCDER, assuming that you can rm multiple paths
     def rm(host_alias, paths)
-      rm_path = 'TODO' # Doesnt exist yet - not implemented in LOBCDER
-
-      request_body = {
+      payload = {
           name: host_alias,
           path: paths
       }
 
-      connection.post do |req| # Only a guess on how could such a request look
-        req.url rm_path
+      @connection.post do |req| # Only a guess on how could such a request look
+        req.url attribute_fetcher('rm_path') # Doesnt exist yet - not implemented in LOBCDER
         req.headers['Content-Type'] = 'application/json'
-        req.body = request_body
+        req.body = payload
       end
     end
 
     def host_aliases
-      folders_path = Rails.application.config_for('process')['staging']['folders_path']
+      folders_response = @connection.get(attribute_fetcher('folders_path'))
+      JSON.parse(folders_response.body, symbolize_names: true).keys
+    end
 
-      folders_response = @connection.get(folders_path)
-      body = JSON.parse(folders_response.body, symbolize_names: false)
+    def copy(commands)
+      copy_move_utility(commands, attribute_fetcher('copy_path'))
+    end
 
-      body.keys
+    def move(commands)
+      copy_move_utility(commands, attribute_fetcher('move_path'))
+    end
+
+    def status(track_id)
+      response = @connection.get "#{attribute_fetcher('status_path')}/#{track_id}"
+      JSON.parse(response.body, symbolize_names: true)[:status] # We don't care about each one file status
+    end
+
+    def list(host_alias, path)
+      payload = {
+          'name': host_alias,
+          'path': path
+      }.to_json
+
+      response = @connection.post do |req|
+        req.url attribute_fetcher('list_path')
+        req.body = payload
+      end
+      JSON.parse(response.body, symbolize_names: true)
     end
 
     private
 
     def get_connection(uc_no)
-      infra_host = Rails.application.config_for('process')['staging']['infra_host']
-      infra_port = Rails.application.config_for('process')['staging']['infra_port']
-      infra_path = Rails.application.config_for('process')['staging']['infra_path']
-      infra_token_header = Rails.application.config_for('process')['staging']['infra_token_header']
-      uc_infra_token = Rails.application.config_for('process')['staging']["uc#{uc_no}_infra_token"]
+      infra_host = attribute_fetcher('infra_host')
+      infra_port = attribute_fetcher('infra_port')
+      infra_path = attribute_fetcher('infra_path')
+      infra_token_header = attribute_fetcher('infra_token_header')
+      uc_infra_token = attribute_fetcher("uc#{uc_no}_infra_token")
 
       infra_resp = Faraday.get("#{infra_host}:#{infra_port}#{infra_path}") do |req|
         req.headers[infra_token_header] = uc_infra_token
@@ -75,6 +93,50 @@ module Staging
         faraday.adapter Faraday.default_adapter
         faraday.headers[token_header] = token
       end
+    end
+
+    # utilities
+    def copy_move_utility(commands, api_path)
+      # commands argument example
+      # [
+      #     {
+      #         'dst': {
+      #             'name': 'lrzcluster',
+      #             'file': '/'
+      #         },
+      #         'src': {
+      #             'name': 'krk',
+      #             'file': '10M.dat'
+      #         }
+      #     }
+      # ]
+      payload = {
+          'id': SecureRandom.hex, # what should we send?
+          'webhook': webhook_info,
+          'cmd': commands
+      }.to_json
+
+      response = @connection.post do |req|
+        req.url attribute_fetcher(api_path)
+        req.body = payload
+      end
+      JSON.parse(response.body)['trackId']
+    end
+
+    def attribute_fetcher(attribute)
+      Rails.application.config_for('process')['staging'][attribute]
+    end
+
+    def webhook_info
+      { method: 'POST',
+        url: webhook_url,
+        headers: { 'x-staging-token': attribute_fetcher('staging_secret'),
+                   'content-type': 'application/json' } }
+    end
+
+    def webhook_url
+      Rails.application.routes.url_helpers.api_staging_url(protocol: 'https',
+                                                           host: ENV['HOST'])
     end
   end
 end
