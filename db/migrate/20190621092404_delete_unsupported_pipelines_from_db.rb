a6dfc40c23143ae8e46f2e0ead77ae790385a067
jan <jankapala@gmail.com>
2020-04-30 16:27:17 +0200

    Rewrite failing migration

--- db/migrate/20190621092404_delete_unsupported_pipelines_from_db.rb
+++ db/migrate/20190621092404_delete_unsupported_pipelines_from_db.rb
@@ -1,5 +1,9 @@
 class DeleteUnsupportedPipelinesFromDb < ActiveRecord::Migration[5.1]
   def change
-    Pipeline.where.not(flow: Flow.types).destroy_all
+    # Delete all pipelines and related records in other relations
+    # (deleting all pipelines => deleting all computations)
+    ActiveRecord::Base.connection.execute("TRUNCATE computations")
+    ActiveRecord::Base.connection.execute("DELETE FROM data_files WHERE input_of_id IS NOT NULL OR output_of_id IS NOT NULL")
+    ActiveRecord::Base.connection.execute("TRUNCATE pipelines")
   end
 end
