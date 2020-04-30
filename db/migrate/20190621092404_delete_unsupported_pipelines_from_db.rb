6e32b5e96ab8de4843cb6470527e8428880199e7
jan <jankapala@gmail.com>
2020-04-30 18:03:36 +0200

    Fix migration approach 3

--- db/migrate/20190621092404_delete_unsupported_pipelines_from_db.rb
+++ db/migrate/20190621092404_delete_unsupported_pipelines_from_db.rb
@@ -2,7 +2,6 @@ class DeleteUnsupportedPipelinesFromDb < ActiveRecord::Migration[5.1]
   def change
     # Delete all pipelines and related records in other relations
     # (deleting all pipelines => deleting all computations)
-    ActiveRecord::Base.connection.execute("TRUNCATE computations")
     ActiveRecord::Base.connection.execute("TRUNCATE pipelines CASCADE")
   end
 end
